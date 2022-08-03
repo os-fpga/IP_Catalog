9b8809ab33c7d3a5a8fca15bfb812b96823d33da
Moin Ijaz <mijaz@indentityrapid.khazanapk.com>
2022-08-03 17:19:59 +0500

    axi_ram generator added

---
+++ RapidSilicon/IP/axi_ram/v1_0/axi_ram_gen.py
@@ -0,0 +1,283 @@
+#!/usr/bin/env python3
+
+# This file is Copyright (c) 2022 RapidSilicon.
+# SPDX-License-Identifier: TBD.
+
+import os
+import json
+import argparse
+import shutil
+from datetime import datetime
+
+from migen import *
+
+from litex.build.generic_platform import *
+from litex.build.osfpga import OSFPGAPlatform
+
+from litex.soc.interconnect import stream
+from litex.soc.interconnect.axi import *
+
+
+# Build --------------------------------------------------------------------------------------------
+def main():
+    parser = argparse.ArgumentParser(description="AXI RAM CORE")
+    parser.formatter_class = lambda prog: argparse.ArgumentDefaultsHelpFormatter(prog,
+        max_help_position = 10,
+        width             = 120
+    )
+
+    # Core Parameters.
+    core_group = parser.add_argument_group(title="Core parameters")
+    core_group.add_argument("--data_width",     default=32,                   help="RAM Data Width 8,16,32")
+    core_group.add_argument("--addr_width",     default=16,                   help="RAM Address Width 8,16")
+    core_group.add_argument("--id_width",       default=8,                    help="RAM ID Width from 0 - 8")
+    core_group.add_argument("--pip_out",        default=0,                    help="RAM Pipeline Output 0 or 1")
+
+
+    # Build Parameters.
+    build_group = parser.add_argument_group(title="Build parameters")
+    build_group.add_argument("--build",         action="store_true",            help="Build Core")
+    build_group.add_argument("--build-dir",     default="build",                help="Build Directory")
+    build_group.add_argument("--build-name",    default="axi_ram_wrapper",      help="Module Name and File Name of the RTL")
+
+    # JSON Import/Template
+    json_group = parser.add_argument_group(title="JSON Parameters")
+    json_group.add_argument("--json",                                           help="Generate Core from JSON File")
+    json_group.add_argument("--json-template",  action="store_true",            help="Generate JSON Template")
+
+    args = parser.parse_args()
+
+    # Import JSON (Optional) -----------------------------------------------------------------------
+    if args.json:
+        with open(args.json, 'rt') as f:
+            t_args = argparse.Namespace()
+            t_args.__dict__.update(json.load(f))
+            args = parser.parse_args(namespace=t_args)
+
+    # Export JSON Template (Optional) --------------------------------------------------------------
+    if args.json_template:
+        print(json.dumps(vars(args), indent=4))
+
+    # Create LiteX Core ----------------------------------------------------------------------------
+    platform   = OSFPGAPlatform("", io=[], toolchain="raptor")
+    rtl_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "src")
+
+    # Remove build extension when specified --------------------------------------------------------
+    args.build_name = os.path.splitext(args.build_name)[0]
+
+    # Build Project Directory ----------------------------------------------------------------------
+    if args.build:
+        # Build Path
+        build_path = os.path.join(os.path.abspath(os.getcwd()), (args.build_name) + '/rapidsilicon/ip/axi_ram/v1_0/')
+        if not os.path.exists(build_path):
+            os.makedirs(build_path)
+
+        # Simulation Path
+        sim_path = os.path.join(build_path, "sim")
+        if not os.path.exists(sim_path):    
+            os.makedirs(sim_path)
+
+        # Source Path
+        src_path = os.path.join(build_path, "src")
+        if not os.path.exists(src_path):    
+            os.makedirs(src_path) 
+
+        # Synthesis Path
+        synth_path = os.path.join(build_path, "synth")
+        if not os.path.exists(synth_path):    
+            os.makedirs(synth_path) 
+        # Design Path
+        design_path = os.path.join("../src", (args.build_name + ".v")) 
+
+        # Copy RTL from Source to Destination        
+        src_files = os.listdir(rtl_path)
+        for file_name in src_files:
+            full_file_name = os.path.join(rtl_path, file_name)
+            if os.path.isfile(full_file_name):
+                shutil.copy(full_file_name, src_path)
+
+        # TCL File Content        
+        tcl = []
+        # Create Design.
+        tcl.append(f"create_design {args.build_name}")
+        # Set Device.
+        tcl.append(f"target_device {'GEMINI'}")
+        # Add Include Path.
+        tcl.append(f"add_library_path {'../src'}")
+        # Add Sources.
+#        for f, typ, lib in file_name:
+        tcl.append(f"add_design_file {design_path}")
+        # Set Top Module.
+        tcl.append(f"set_top_module {args.build_name}")
+        # Add Timings Constraints.
+#        tcl.append(f"add_constraint_file {args.build_name}.sdc")
+        # Run.
+        tcl.append("synthesize")
+
+        # Generate .tcl file
+        tcl_path = os.path.join(synth_path, "raptor.tcl")
+        with open(tcl_path, "w") as f:
+            f.write("\n".join(tcl))
+        f.close()
+
+        # Generate RTL Wrapper
+        wrapper_path = os.path.join(src_path, (args.build_name + ".v"))
+        with open(wrapper_path, "w") as f:
+
+#-------------------------------------------------------------------------------
+# ------------------------- RTL WRAPPER ----------------------------------------
+#-------------------------------------------------------------------------------
+            f.write ("""
+/////////////////////////////////////////////////////////////////////////////////////////
+// For Reference: https://github.com/alexforencich/verilog-axi/blob/master/rtl/axi_ram.v
+/////////////////////////////////////////////////////////////////////////////////////////
+/*
+Copyright (c) 2018 Alex Forencich
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+/////////////////////////////////////////////////////////////////////////////////////////\n\n
+""")
+            f.write ("// Created on: {}\n// Language: Verilog 2001\n\n".format(datetime.now()))
+            f.write ("`resetall \n`timescale 1ns/ 1ps \n`default_nettype none \n\n")
+            f.write ("""module {} #(""".format(args.build_name))
+
+            f.write ("""
+    // Width of data bus in bits
+    localparam DATA_WIDTH = {}, """.format(args.data_width))
+
+            f.write ("""
+    // Width of address bus in bits
+    localparam ADDR_WIDTH = {}, """.format(args.addr_width))
+
+            f.write ("""
+    // Width of ID from 0 - 8
+    localparam ID_WIDTH = {}, """.format(args.id_width))
+
+            f.write ("""
+    // Extra pipeline register on output
+    localparam PIPELINE_OUTPUT = {}, """.format(args.pip_out))
+
+            f.write("""
+    // Width of wstrb (width of data bus in words)
+    localparam STRB_WIDTH = (DATA_WIDTH/8)
+)
+(
+    input  wire                   clk,
+    input  wire                   rst,
+
+    input  wire [ID_WIDTH-1:0]    s_axi_awid,
+    input  wire [ADDR_WIDTH-1:0]  s_axi_awaddr,
+    input  wire [7:0]             s_axi_awlen,
+    input  wire [2:0]             s_axi_awsize,
+    input  wire [1:0]             s_axi_awburst,
+    input  wire                   s_axi_awlock,
+    input  wire [3:0]             s_axi_awcache,
+    input  wire [2:0]             s_axi_awprot,
+    input  wire                   s_axi_awvalid,
+    output wire                   s_axi_awready,
+
+    input  wire [DATA_WIDTH-1:0]  s_axi_wdata,
+    input  wire [STRB_WIDTH-1:0]  s_axi_wstrb,
+    input  wire                   s_axi_wlast,
+    input  wire                   s_axi_wvalid,
+    output wire                   s_axi_wready,
+
+    output wire [ID_WIDTH-1:0]    s_axi_bid,
+    output wire [1:0]             s_axi_bresp,
+    output wire                   s_axi_bvalid,
+    input  wire                   s_axi_bready,
+
+    input  wire [ID_WIDTH-1:0]    s_axi_arid,
+    input  wire [ADDR_WIDTH-1:0]  s_axi_araddr,
+    input  wire [7:0]             s_axi_arlen,
+    input  wire [2:0]             s_axi_arsize,
+    input  wire [1:0]             s_axi_arburst,
+    input  wire                   s_axi_arlock,
+    input  wire [3:0]             s_axi_arcache,
+    input  wire [2:0]             s_axi_arprot,
+    input  wire                   s_axi_arvalid,
+    output wire                   s_axi_arready,
+
+    output wire [ID_WIDTH-1:0]    s_axi_rid,
+    output wire [DATA_WIDTH-1:0]  s_axi_rdata,
+    output wire [1:0]             s_axi_rresp,
+    output wire                   s_axi_rlast,
+    output wire                   s_axi_rvalid,
+    input  wire                   s_axi_rready
+);
+
+""")
+            f.write("axi_ram #(\n.DATA_WIDTH(DATA_WIDTH),\n.ADDR_WIDTH(ADDR_WIDTH),\n.ID_WIDTH(ID_WIDTH),\n.PIPELINE_OUTPUT(PIPELINE_OUTPUT)\n)")
+            f.write("""
+
+ram_inst
+(
+.clk(clk),
+.rst(rst),
+
+.s_axi_awid(s_axi_awid),
+.s_axi_awaddr(s_axi_awaddr),
+.s_axi_awlen(s_axi_awlen),
+.s_axi_awsize(s_axi_awsize),
+.s_axi_awburst(s_axi_awburst),
+.s_axi_awlock(s_axi_awlock),
+.s_axi_awcache(s_axi_awcache),
+.s_axi_awprot(s_axi_awprot),
+.s_axi_awvalid(s_axi_awvalid),
+.s_axi_awready(s_axi_awready),
+
+.s_axi_wdata(s_axi_wdata),
+.s_axi_wstrb(s_axi_wstrb),
+.s_axi_wlast(s_axi_wlast),
+.s_axi_wvalid(s_axi_wvalid),
+.s_axi_wready(s_axi_wready),
+
+.s_axi_bid(s_axi_bid),
+.s_axi_bresp(s_axi_bresp),
+.s_axi_bvalid(s_axi_bvalid),
+.s_axi_bready(s_axi_bready),
+
+.s_axi_arid(s_axi_arid),
+.s_axi_araddr(s_axi_araddr),
+.s_axi_arlen(s_axi_arlen),
+.s_axi_arsize(s_axi_arsize),
+.s_axi_arburst(s_axi_arburst),
+.s_axi_arlock(s_axi_arlock),
+.s_axi_arcache(s_axi_arcache),
+.s_axi_arprot(s_axi_arprot),
+.s_axi_arvalid(s_axi_arvalid),
+.s_axi_arready(s_axi_arready),
+
+.s_axi_rid(s_axi_rid),
+.s_axi_rdata(s_axi_rdata),
+.s_axi_rresp(s_axi_rresp),
+.s_axi_rlast(s_axi_rlast),
+.s_axi_rvalid(s_axi_rvalid),
+.s_axi_rready(s_axi_rready)
+);
+
+endmodule
+
+`resetall
+             """)
+            f.close()
+
+if __name__ == "__main__":
+    main()
