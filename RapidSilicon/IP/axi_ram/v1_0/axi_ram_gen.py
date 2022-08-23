f193af6df761c6568c38507e4eba0096216de1aa
Moin Ijaz <mijaz@indentityrapid.khazanapk.com>
2022-08-23 18:04:57 +0500

    generator file creation fixed

--- RapidSilicon/IP/axi_ram/v1_0/axi_ram_gen.py
+++ RapidSilicon/IP/axi_ram/v1_0/axi_ram_gen.py
@@ -55,7 +55,8 @@ def main():
     if args.build:
         # Build Path
         build_path = os.path.join(args.build_dir, 'ip_build/rapidsilicon/ip/axi_ram/v1_0/' + (args.mod_name))
-        gen_path = os.path.join("axi_ram_gen.py")
+        gen_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "axi_ram_gen.py"))
+
         if not os.path.exists(build_path):
             os.makedirs(build_path)
             shutil.copy(gen_path, build_path)
