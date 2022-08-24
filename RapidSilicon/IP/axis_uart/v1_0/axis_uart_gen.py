288fcedf49cb03b57852da40e6e0fc0d72598ac3
Moin Ijaz <mijaz@indentityrapid.khazanapk.com>
2022-08-24 15:10:03 +0500

    axis_uart added

---
+++ RapidSilicon/IP/axis_uart/v1_0/axis_uart_gen.py
@@ -0,0 +1,240 @@
+#!/usr/bin/env python3
+
+# This file is Copyright (c) 2022 RapidSilicon.
+# SPDX-License-Identifier: TBD.
+
+import os
+import json
+import argparse
+import shutil
+from datetime import datetime
+
+# Build --------------------------------------------------------------------------------------------
+def main():
+    parser = argparse.ArgumentParser(description="AXIS UART CORE")
+    parser.formatter_class = lambda prog: argparse.ArgumentDefaultsHelpFormatter(prog,
+        max_help_position = 10,
+        width             = 120
+    )
+
+    # Core Parameters.
+    core_group = parser.add_argument_group(title="Core parameters")
+    core_group.add_argument("--data_width",     default=5,                      help="UART Data Width 5 - 8")
+
+    # Build Parameters.
+    build_group = parser.add_argument_group(title="Build parameters")
+    build_group.add_argument("--build",         action="store_true",            help="Build Core")
+    build_group.add_argument("--build-dir",     default="",                     help="Build Directory")
+    build_group.add_argument("--build-name",    default="axis_uart_wrapper",    help="Build Folder Name, Build RTL File Name and Module Name")
+
+    # JSON Import/Template
+    json_group = parser.add_argument_group(title="JSON Parameters")
+    json_group.add_argument("--json",                                           help="Generate Core from JSON File")
+    json_group.add_argument("--json-template",  action="store_true",            help="Generate JSON Template")
+
+    args = parser.parse_args()
+
+    # Import JSON (Optional) -----------------------------------------------------------------------
+    if args.json:
+        with open(args.json, 'rt') as f:
+            t_args = argparse.Namespace()
+            t_args.__dict__.update(json.load(f))
+            args = parser.parse_args(namespace=t_args)
+
+    # Export JSON Template (Optional) --------------------------------------------------------------
+    if args.json_template:
+        print(json.dumps(vars(args), indent=4))
+
+    # Remove build extension when specified.
+    args.build_name = os.path.splitext(args.build_name)[0]
+
+    # Build Project Directory ----------------------------------------------------------------------
+    if args.build:
+        # Build Path
+        build_path = os.path.join(args.build_dir, 'ip_build/rapidsilicon/ip/axis_uart/v1_0/' + (args.build_name))
+        gen_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "axis_uart_gen.py"))
+        
+        if not os.path.exists(build_path):
+            os.makedirs(build_path)
+            shutil.copy(gen_path, build_path)
+
+        # Litex_sim Path
+        litex_sim_path = os.path.join(build_path, "litex_sim")
+        if not os.path.exists(litex_sim_path):    
+            os.makedirs(litex_sim_path)
+
+        # Simulation Path
+        sim_path = os.path.join(build_path, "sim")
+        if not os.path.exists(sim_path):    
+            os.makedirs(sim_path)
+
+        # Source Path
+        src_path = os.path.join(build_path, "src")
+        if not os.path.exists(src_path):    
+            os.makedirs(src_path) 
+
+        # Synthesis Path
+        synth_path = os.path.join(build_path, "synth")
+        if not os.path.exists(synth_path):    
+            os.makedirs(synth_path) 
+
+        # Design Path
+        design_path = os.path.join("../src", (args.build_name + ".v")) 
+
+        # Copy RTL from Source to Destination
+        rtl_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "src")        
+        rtl_files = os.listdir(rtl_path)
+        for file_name in rtl_files:
+            full_file_path = os.path.join(rtl_path, file_name)
+            if os.path.isfile(full_file_path):
+                shutil.copy(full_file_path, src_path)
+
+        # Copy litex_sim Data from Source to Destination  
+        litex_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "litex_sim")      
+        litex_files = os.listdir(litex_path)
+        for file_name in litex_files:
+            full_file_path = os.path.join(litex_path, file_name)
+            if os.path.isfile(full_file_path):
+                shutil.copy(full_file_path, litex_sim_path)
+
+        
+        # TCL File Content        
+        tcl = []
+        # Create Design.
+        tcl.append(f"create_design {args.build_name}")
+        # Set Device.
+        tcl.append(f"target_device {'GEMINI'}")
+        # Add Include Path.
+        tcl.append(f"add_library_path {'../src'}")
+        # Add Sources.
+#        for f, typ, lib in file_name:
+        tcl.append(f"add_design_file {design_path}")
+        # Set Top Module.
+        tcl.append(f"set_top_module {args.build_name}")
+        # Add Timings Constraints.
+#        tcl.append(f"add_constraint_file {args.build_name}.sdc")
+        # Run.
+        tcl.append("synthesize")
+        tcl.append("packing")
+        tcl.append("global_placement")
+        tcl.append("place")
+        tcl.append("route")
+        tcl.append("sta")
+        tcl.append("power")
+        tcl.append("bitstream")
+
+        # Generate .tcl file
+        tcl_path = os.path.join(synth_path, "raptor.tcl")
+        with open(tcl_path, "w") as f:
+            f.write("\n".join(tcl))
+        f.close()
+
+        # Generate RTL Wrapper
+        if args.build_name:
+            wrapper_path = os.path.join(src_path, (args.build_name + ".v"))
+            with open(wrapper_path, "w") as f:
+
+#-------------------------------------------------------------------------------
+# ------------------------- RTL WRAPPER ----------------------------------------
+#-------------------------------------------------------------------------------
+                f.write ("""
+///////////////////////////////////////////////////////////////////////////////////////
+// For Reference: https://github.com/alexforencich/verilog-uart/blob/master/rtl/uart.v
+///////////////////////////////////////////////////////////////////////////////////////
+/*
+Copyright (c) 2014-2017 Alex Forencich
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+///////////////////////////////////////////////////////////////////////////////////////\n\n
+""")
+                f.write ("// Created on: {}\n// Language: Verilog 2001\n\n".format(datetime.now()))
+                f.write ("`resetall \n`timescale 1ns/ 1ps \n`default_nettype none \n\n")
+                f.write ("""module {} #(""".format(args.build_name))
+
+                f.write ("""
+    // Width of data bus in bits
+    localparam DATA_WIDTH = {} """.format(args.data_width))
+
+                f.write("""
+)
+(
+    input  wire                   clk,
+    input  wire                   rst,
+
+    // AXI input
+    input  wire [DATA_WIDTH-1:0]  s_axis_tdata,
+    input  wire                   s_axis_tvalid,
+    output wire                   s_axis_tready,
+
+    // AXI output
+    output wire [DATA_WIDTH-1:0]  m_axis_tdata,
+    output wire                   m_axis_tvalid,
+    input  wire                   m_axis_tready,
+
+    // UART interface
+    input  wire                   rxd,
+    output wire                   txd,
+
+    // Status
+    output wire                   tx_busy,
+    output wire                   rx_busy,
+    output wire                   rx_overrun_error,
+    output wire                   rx_frame_error,
+
+    // Configuration
+    input  wire [15:0]            prescale
+);
+
+""")
+                f.write("uart #(\n.DATA_WIDTH(DATA_WIDTH)\n)")
+                f.write("""
+
+uart_inst
+(
+.clk(clk),
+.rst(rst),
+
+.s_axis_tdata(s_axis_tdata),
+.s_axis_tvalid(s_axis_tvalid),
+.s_axis_tready(s_axis_tready),
+
+.m_axis_tdata(m_axis_tdata),
+.m_axis_tvalid(m_axis_tvalid),
+.m_axis_tready(m_axis_tready),
+
+.rxd(rxd),
+.txd(txd),
+
+.tx_busy(tx_busy),
+.rx_busy(rx_busy),
+.rx_overrun_error(rx_overrun_error),
+.rx_frame_error(rx_frame_error),
+
+.prescale(prescale)
+);
+
+endmodule
+
+`resetall
+             """)
+                f.close()
+
+if __name__ == "__main__":
+    main()
