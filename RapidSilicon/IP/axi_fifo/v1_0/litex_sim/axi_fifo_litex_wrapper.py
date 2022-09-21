ef91166c2008edb32712c58ce2ef285084f213f2
Moin Ijaz <mijaz@indentityrapid.khazanapk.com>
2022-09-21 19:26:32 +0500

    minor changes as per litex

--- RapidSilicon/IP/axi_fifo/v1_0/litex_sim/axi_fifo_litex_wrapper.py
+++ RapidSilicon/IP/axi_fifo/v1_0/litex_sim/axi_fifo_litex_wrapper.py
@@ -37,25 +37,38 @@ class AXIFIFO(Module):
         
         # Clock Domain.
         clock_domain = s_axi.clock_domain
-        self.logger.info(f"Clock Domain: {clock_domain}")
+        self.logger.info(f"Clock Domain     : {clock_domain}")
 
         # Parameters
         data_width = len(s_axi.w.data)
         self.logger.info(f"DATA_WIDTH       : {data_width}")
+        
         addr_width = len(s_axi.aw.addr)
         self.logger.info(f"ADDR_WIDTH       : {addr_width}")
+        
         id_width = len(s_axi.ar.id)
         self.logger.info(f"ID_WIDTH         : {id_width}")
+        
         self.logger.info(f"AWUSER_ENABLE    : {aw_user_en}")
+        aw_user_width = len(s_axi.aw.user)
         self.logger.info(f"AWUSER_WIDTH     : {aw_user_width}")
+        
         self.logger.info(f"WUSER_ENABLE     : {w_user_en}")
+        w_user_width = len(s_axi.w.user)
         self.logger.info(f"WUSER_WIDTH      : {w_user_width}")
+        
         self.logger.info(f"BUSER_ENABLE     : {b_user_en}")
+        b_user_width = len(s_axi.b.user)
         self.logger.info(f"BUSER_WIDTH      : {b_user_width}")
+        
         self.logger.info(f"ARUSER_ENABLE    : {ar_user_en}")
+        ar_user_width = len(s_axi.ar.user)
         self.logger.info(f"ARUSER_WIDTH     : {ar_user_width}")
+        
         self.logger.info(f"RUSER_ENABLE     : {r_user_en}")
+        r_user_width = len(s_axi.r.user)
         self.logger.info(f"RUSER_WIDTH      : {r_user_width}")
+        
         self.logger.info(f"WRITE_FIFO_DEPTH : {write_fifo_depth}")
         self.logger.info(f"READ_FIFO_DEPTH  : {read_fifo_depth}")
         self.logger.info(f"WRITE_FIFO_DELAY : {write_fifo_delay}")
@@ -102,18 +115,18 @@ class AXIFIFO(Module):
             i_s_axi_awprot      = s_axi.aw.prot,
             i_s_axi_awqos       = s_axi.aw.qos,
             i_s_axi_awregion    = s_axi.aw.region,
-            # i_s_axi_awuser      = s_axi.aw.user,
+            i_s_axi_awuser      = s_axi.aw.user,
             i_s_axi_awvalid     = s_axi.aw.valid,
             o_s_axi_awready     = s_axi.aw.ready,
             i_s_axi_wdata       = s_axi.w.data,
             i_s_axi_wstrb       = s_axi.w.strb,
             i_s_axi_wlast       = s_axi.w.last,
-            # i_s_axi_wuser       = s_axi.w.user,
+            i_s_axi_wuser       = s_axi.w.user,
             i_s_axi_wvalid      = s_axi.w.valid,
             o_s_axi_wready      = s_axi.w.ready,
             o_s_axi_bid         = s_axi.b.id,
             o_s_axi_bresp       = s_axi.b.resp,
-            # o_s_axi_buser       = s_axi.b.user,
+            o_s_axi_buser       = s_axi.b.user,
             o_s_axi_bvalid      = s_axi.b.valid,
             i_s_axi_bready      = s_axi.b.ready,
             i_s_axi_arid        = s_axi.ar.id,
@@ -126,14 +139,14 @@ class AXIFIFO(Module):
             i_s_axi_arprot      = s_axi.ar.prot,
             i_s_axi_arqos       = s_axi.ar.qos,
             i_s_axi_arregion    = s_axi.ar.region,
-            # i_s_axi_aruser      = s_axi.ar.user,
+            i_s_axi_aruser      = s_axi.ar.user,
             i_s_axi_arvalid     = s_axi.ar.valid,
             o_s_axi_arready     = s_axi.ar.ready,
             o_s_axi_rid         = s_axi.r.id,
             o_s_axi_rdata       = s_axi.r.data,
             o_s_axi_rresp       = s_axi.r.resp,
             o_s_axi_rlast       = s_axi.r.last,
-            # o_s_axi_ruser       = s_axi.r.user,
+            o_s_axi_ruser       = s_axi.r.user,
             o_s_axi_rvalid      = s_axi.r.valid,
             i_s_axi_rready      = s_axi.r.ready,
             
@@ -150,18 +163,18 @@ class AXIFIFO(Module):
             o_m_axi_awprot      = m_axi.aw.prot,
             o_m_axi_awqos       = m_axi.aw.qos,
             o_m_axi_awregion    = m_axi.aw.region,
-            # o_m_axi_awuser      = m_axi.aw.user,
+            o_m_axi_awuser      = m_axi.aw.user,
             o_m_axi_awvalid     = m_axi.aw.valid,
             i_m_axi_awready     = m_axi.aw.ready,
             o_m_axi_wdata       = m_axi.w.data,
             o_m_axi_wstrb       = m_axi.w.strb,
             o_m_axi_wlast       = m_axi.w.last,
-            # o_m_axi_wuser       = m_axi.w.user,
+            o_m_axi_wuser       = m_axi.w.user,
             o_m_axi_wvalid      = m_axi.w.valid,
             i_m_axi_wready      = m_axi.w.ready,
             i_m_axi_bid         = m_axi.b.id,
             i_m_axi_bresp       = m_axi.b.resp,
-            # i_m_axi_buser       = m_axi.b.user,
+            i_m_axi_buser       = m_axi.b.user,
             i_m_axi_bvalid      = m_axi.b.valid,
             o_m_axi_bready      = m_axi.b.ready,
             o_m_axi_arid        = m_axi.ar.id,
@@ -174,14 +187,14 @@ class AXIFIFO(Module):
             o_m_axi_arprot      = m_axi.ar.prot,
             o_m_axi_arqos       = m_axi.ar.qos,
             o_m_axi_arregion    = m_axi.ar.region,
-            # o_m_axi_aruser      = m_axi.ar.user,
+            o_m_axi_aruser      = m_axi.ar.user,
             o_m_axi_arvalid     = m_axi.ar.valid,
             i_m_axi_arready     = m_axi.ar.ready,
             i_m_axi_rid         = m_axi.r.id,
             i_m_axi_rdata       = m_axi.r.data,
             i_m_axi_rresp       = m_axi.r.resp,
             i_m_axi_rlast       = m_axi.r.last,
-            # i_m_axi_ruser       = m_axi.r.user,
+            i_m_axi_ruser       = m_axi.r.user,
             i_m_axi_rvalid      = m_axi.r.valid,
             o_m_axi_rready      = m_axi.r.ready            
         )
