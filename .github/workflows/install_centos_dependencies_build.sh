cf0606c99fbae8149708b0a06ae21eba60500d8d
nadeemyaseen-rs <86344264+nadeemyaseen-rs@users.noreply.github.com>
2022-09-22 03:18:14 +0500

    Update install_centos_dependencies_build.sh

--- .github/workflows/install_centos_dependencies_build.sh
+++ .github/workflows/install_centos_dependencies_build.sh
@@ -2,3 +2,4 @@
 curl -C - -O https://cmake.org/files/v3.15/cmake-3.15.7-Linux-x86_64.tar.gz
 tar xzf cmake-3.15.7-Linux-x86_64.tar.gz
 ln -s $PWD/cmake-3.15.7-Linux-x86_64/bin/cmake /usr/bin/cmake
+yum install -y wget
