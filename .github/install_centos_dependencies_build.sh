05cee47a4db2c49f3aa69824f113e1bb3d29ccf7
ravic-rs <108307503+ravic-rs@users.noreply.github.com>
2022-09-28 01:29:29 -0700

    Update install_centos_dependencies_build.sh

--- .github/install_centos_dependencies_build.sh
+++ .github/install_centos_dependencies_build.sh
@@ -2,7 +2,7 @@ yum group install -y "Development Tools"
 yum install -y python3 which tree
 python3 -m pip install --upgrade pip
 python3 -m pip install pipenv
+python3 -m pip install wheel
 curl -C - -O https://cmake.org/files/v3.15/cmake-3.15.7-Linux-x86_64.tar.gz
 tar xvzf cmake-3.15.7-Linux-x86_64.tar.gz
 ln -s $PWD/cmake-3.15.7-Linux-x86_64/bin/cmake /usr/bin/cmake
-python3 -m pip install wheel
