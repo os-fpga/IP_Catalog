0fae70cd5d256a38a54af0bbf620bf670b790a5c
Moin ijaz <moin.ijaz@rslpt42.rapidsilicon.local>
2022-10-06 20:33:15 +0500

    IOs updated as per litex environment and code clean-up

--- RapidSilicon/IP/axi_vexriscv_cpu/v1_0/litex_sim/axi_vexriscv_litex_wrapper.py
+++ RapidSilicon/IP/axi_vexriscv_cpu/v1_0/litex_sim/axi_vexriscv_litex_wrapper.py
@@ -1,77 +1,18 @@
-
+#
 # This file is part of RapidSilicon's IP_Catalog.
 #
 # This file is Copyright (c) 2022 RapidSilicon.
-# SPDX-License-Identifier: MIT.
-
+#
+# SPDX-License-Identifier: MIT
+#
 # LiteX wrapper around SPinalHDL VexRiscvAxi4.v
 
 import os
-import logging
+
 from migen import *
-from litex.soc.interconnect import axi
-from litex import get_data_mod
-from litex.soc.interconnect import wishbone
-from litex.soc.interconnect.csr import *
-from litex.soc.cores.cpu import CPU, CPU_GCC_TRIPLE_RISCV32
 
-
-logging.basicConfig(level=logging.INFO)
-
-# VexRiscv Timer -----------------------------------------------------------------------------------
-
-class VexRiscvTimer(Module, AutoCSR):
-    def __init__(self):
-        self._latch    = CSR()
-        self._time     = CSRStatus(64)
-        self._time_cmp = CSRStorage(64, reset=2**64-1)
-        self.interrupt = Signal()
-
-        # # #
-
-        time = Signal(64)
-        self.sync += time.eq(time + 1)
-        self.sync += If(self._latch.re, self._time.status.eq(time))
-
-        time_cmp = Signal(64, reset=2**64-1)
-        self.sync += If(self._latch.re, time_cmp.eq(self._time_cmp.storage))
-
-        self.comb += self.interrupt.eq(time >= time_cmp)
-
-# VexRiscv -----------------------------------------------------------------------------------------
-class Open(Signal): pass
-
-class VexRiscv(CPU, AutoCSR):
-    family               = "riscv"
-    name                 = "vexriscv"
-    human_name           = "VexRiscv"
-#    variants             = CPU_VARIANTS
-    data_width           = 32
-    endianness           = "little"
-    gcc_triple           = CPU_GCC_TRIPLE_RISCV32
-    linker_output_format = "elf32-littleriscv"
-    nop                  = "nop"
-    io_regions           = {0x80000000: 0x80000000} # Origin, Length
-
-    # Memory Mapping.
-    @property
-    def mem_map(self):
-        return {
-            "rom":            0x00000000,
-            "sram":           0x10000000,
-            "main_ram":       0x40000000,
-            "csr":            0xf0000000,
-            "vexriscv_debug": 0xf00f0000,
-        }
-
-    # GCC Flags.
-    @property
-    def gcc_flags(self):
-        flags = GCC_FLAGS[self.variant]
-        flags += " -D__vexriscv__ -DUART_POLLING"
-        return flags
-
-    def __init__(self, platform, ibus, dbus,  variant="standard", with_timer=False):
+class VexRiscv(Module):
+    def __init__(self, platform, ibus, dbus, variant="standard"):
         self.platform         = platform
         self.variant          = variant
         self.human_name       = "VexRiscv"
@@ -80,8 +21,6 @@ class VexRiscv(CPU, AutoCSR):
         self.dbus             = dbus
         self.reset            = Signal()
         self.interrupt        = Signal(32)
-
-        # # #
 
         # Jtag Signals
         self.jtag_tms         = Signal()
@@ -113,13 +52,13 @@ class VexRiscv(CPU, AutoCSR):
             o_debug_resetOut        = self.debug_resetOut,  
 
             # IBUS AXI-FULL   
-
+            # -------------
             # AR.
             o_iBusAxi_ar_valid          = self.ibus.ar.valid,
             i_iBusAxi_ar_ready          = self.ibus.ar.ready,
             o_iBusAxi_ar_payload_addr   = self.ibus.ar.addr,
             o_iBusAxi_ar_payload_id     = self.ibus.ar.id,
-            o_iBusAxi_ar_payload_region = Open(),
+            o_iBusAxi_ar_payload_region = self.ibus.ar.region,
             o_iBusAxi_ar_payload_len    = self.ibus.ar.len,
             o_iBusAxi_ar_payload_size   = self.ibus.ar.size,
             o_iBusAxi_ar_payload_burst  = self.ibus.ar.burst,
@@ -136,12 +75,14 @@ class VexRiscv(CPU, AutoCSR):
             i_iBusAxi_r_payload_resp    = self.ibus.r.resp,
             i_iBusAxi_r_payload_last    = self.ibus.r.last,
 
+            # DBUS AXI-FULL
+            # -------------
             # AW.
             o_dBusAxi_aw_valid          = self.dbus.aw.valid,
             i_dBusAxi_aw_ready          = self.dbus.aw.ready,
             o_dBusAxi_aw_payload_addr   = self.dbus.aw.addr,
             o_dBusAxi_aw_payload_id     = self.dbus.aw.id,
-            o_dBusAxi_aw_payload_region = Open(),
+            o_dBusAxi_aw_payload_region = self.dbus.aw.region,
             o_dBusAxi_aw_payload_len    = self.dbus.aw.len,
             o_dBusAxi_aw_payload_size   = self.dbus.aw.size,
             o_dBusAxi_aw_payload_burst  = self.dbus.aw.burst,
@@ -168,7 +109,7 @@ class VexRiscv(CPU, AutoCSR):
             i_dBusAxi_ar_ready          = self.dbus.ar.ready,
             o_dBusAxi_ar_payload_addr   = self.dbus.ar.addr,
             o_dBusAxi_ar_payload_id     = self.dbus.ar.id,
-            o_dBusAxi_ar_payload_region = Open(),
+            o_dBusAxi_ar_payload_region = self.dbus.ar.region,
             o_dBusAxi_ar_payload_len    = self.dbus.ar.len,
             o_dBusAxi_ar_payload_size   = self.dbus.ar.size,
             o_dBusAxi_ar_payload_burst  = self.dbus.ar.burst,
@@ -181,9 +122,9 @@ class VexRiscv(CPU, AutoCSR):
             i_dBusAxi_r_valid           = self.dbus.r.valid,
             o_dBusAxi_r_ready           = self.dbus.r.ready,
             i_dBusAxi_r_payload_data    = self.dbus.r.data,
-            i_dBusAxi_r_payload_id      = self.dbus.ar.id,
+            i_dBusAxi_r_payload_id      = self.dbus.r.id,
             i_dBusAxi_r_payload_resp    = self.dbus.r.resp,
-            i_dBusAxi_r_payload_last    = self.dbus.ar.last,
+            i_dBusAxi_r_payload_last    = self.dbus.r.last,
 
             # JTAG
             i_jtag_tms= self.jtag_tms,
@@ -201,14 +142,5 @@ class VexRiscv(CPU, AutoCSR):
         rtl_dir = os.path.join(os.path.dirname(__file__), "../src")
         platform.add_source(os.path.join(rtl_dir, "VexRiscvAxi4.v"))
 
-        # Pass I/D Caches info to software.
-        base_variant = str(self.variant.split('+')[0])
-        # DCACHE is present on all variants except minimal and lite.
-        if not base_variant in ["minimal", "lite"]:
-            soc.add_config("CPU_HAS_DCACHE")
-        # ICACHE is present on all variants except minimal.
-        if not base_variant in ["minimal"]:
-            soc.add_config("CPU_HAS_ICACHE")
-
     def do_finalize(self):
-        self.specials += Instance("VexRiscv", **self.cpu_params)
+        self.specials += Instance("VexRiscvAxi4", **self.cpu_params)
