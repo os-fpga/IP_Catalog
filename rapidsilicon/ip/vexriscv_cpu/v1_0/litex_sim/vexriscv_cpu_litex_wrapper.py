e202bf6cebb8299df595a2e8cb5acd83d5d17ae3
Moin ijaz <moin.ijaz@rslpt42.rapidsilicon.local>
2022-10-10 12:06:33 +0500

    vexriscv_cpu renamed

---
+++ rapidsilicon/ip/vexriscv_cpu/v1_0/litex_sim/vexriscv_cpu_litex_wrapper.py
@@ -0,0 +1,146 @@
+#
+# This file is part of RapidSilicon's IP_Catalog.
+#
+# This file is Copyright (c) 2022 RapidSilicon.
+#
+# SPDX-License-Identifier: MIT
+#
+# LiteX wrapper around SPinalHDL Vexriscv.v
+
+import os
+
+from migen import *
+
+class VexRiscv(Module):
+    def __init__(self, platform, ibus, dbus, variant="standard"):
+        self.platform         = platform
+        self.variant          = variant
+        self.human_name       = "VexRiscv"
+        self.external_variant = None
+        self.ibus             = ibus
+        self.dbus             = dbus
+        self.reset            = Signal()
+        self.interrupt        = Signal(32)
+
+        # Jtag Signals
+        self.jtag_tms         = Signal()
+        self.jtag_tdi         = Signal()
+        self.jtag_tdo         = Signal()
+        self.jtag_tck         = Signal()
+
+        # Other IOs
+        self.timerInterrupt   = Signal()
+        self.externalInterrupt= Signal()
+        self.softwareInterrupt= Signal()
+        self.debugReset       = Signal()
+        self.debug_resetOut   = Signal()    
+
+
+        # CPU Instance.
+        self.cpu_params = dict(
+
+            # Clk / Rst.
+            # ----------
+            i_clk                    = ClockSignal("sys"),
+            i_reset                  = ResetSignal("sys") | self.reset,
+
+            #Interrupts/Debug
+            i_externalInterrupt     = self.timerInterrupt,
+            i_timerInterrupt        = self.externalInterrupt,
+            i_softwareInterrupt     = self.softwareInterrupt,
+            i_debugReset            = self.debugReset,
+            o_debug_resetOut        = self.debug_resetOut,  
+
+            # IBUS AXI-FULL   
+            # -------------
+            # AR.
+            o_iBusAxi_ar_valid          = self.ibus.ar.valid,
+            i_iBusAxi_ar_ready          = self.ibus.ar.ready,
+            o_iBusAxi_ar_payload_addr   = self.ibus.ar.addr,
+            o_iBusAxi_ar_payload_id     = self.ibus.ar.id,
+            o_iBusAxi_ar_payload_region = self.ibus.ar.region,
+            o_iBusAxi_ar_payload_len    = self.ibus.ar.len,
+            o_iBusAxi_ar_payload_size   = self.ibus.ar.size,
+            o_iBusAxi_ar_payload_burst  = self.ibus.ar.burst,
+            o_iBusAxi_ar_payload_lock   = self.ibus.ar.lock,
+            o_iBusAxi_ar_payload_cache  = self.ibus.ar.cache,
+            o_iBusAxi_ar_payload_qos    = self.ibus.ar.qos,
+            o_iBusAxi_ar_payload_prot   = self.ibus.ar.prot,
+
+            # R.
+            i_iBusAxi_r_valid           = self.ibus.r.valid,
+            o_iBusAxi_r_ready           = self.ibus.r.ready,
+            i_iBusAxi_r_payload_data    = self.ibus.r.data,
+            i_iBusAxi_r_payload_id      = self.ibus.r.id,
+            i_iBusAxi_r_payload_resp    = self.ibus.r.resp,
+            i_iBusAxi_r_payload_last    = self.ibus.r.last,
+
+            # DBUS AXI-FULL
+            # -------------
+            # AW.
+            o_dBusAxi_aw_valid          = self.dbus.aw.valid,
+            i_dBusAxi_aw_ready          = self.dbus.aw.ready,
+            o_dBusAxi_aw_payload_addr   = self.dbus.aw.addr,
+            o_dBusAxi_aw_payload_id     = self.dbus.aw.id,
+            o_dBusAxi_aw_payload_region = self.dbus.aw.region,
+            o_dBusAxi_aw_payload_len    = self.dbus.aw.len,
+            o_dBusAxi_aw_payload_size   = self.dbus.aw.size,
+            o_dBusAxi_aw_payload_burst  = self.dbus.aw.burst,
+            o_dBusAxi_aw_payload_lock   = self.dbus.aw.lock,
+            o_dBusAxi_aw_payload_cache  = self.dbus.aw.cache,
+            o_dBusAxi_aw_payload_qos    = self.dbus.aw.qos,
+            o_dBusAxi_aw_payload_prot   = self.dbus.aw.prot,
+            
+            # W.
+            o_dBusAxi_w_valid           = self.dbus.w.valid,
+            i_dBusAxi_w_ready           = self.dbus.w.ready,
+            o_dBusAxi_w_payload_data    = self.dbus.w.data,
+            o_dBusAxi_w_payload_strb    = self.dbus.w.strb,
+            o_dBusAxi_w_payload_last    = self.dbus.w.last,
+            
+            # B.
+            i_dBusAxi_b_valid           = self.dbus.b.valid,
+            o_dBusAxi_b_ready           = self.dbus.b.ready,
+            i_dBusAxi_b_payload_id      = self.dbus.b.id,
+            i_dBusAxi_b_payload_resp    = self.dbus.b.resp,
+
+            # AR.
+            o_dBusAxi_ar_valid          = self.dbus.ar.valid,
+            i_dBusAxi_ar_ready          = self.dbus.ar.ready,
+            o_dBusAxi_ar_payload_addr   = self.dbus.ar.addr,
+            o_dBusAxi_ar_payload_id     = self.dbus.ar.id,
+            o_dBusAxi_ar_payload_region = self.dbus.ar.region,
+            o_dBusAxi_ar_payload_len    = self.dbus.ar.len,
+            o_dBusAxi_ar_payload_size   = self.dbus.ar.size,
+            o_dBusAxi_ar_payload_burst  = self.dbus.ar.burst,
+            o_dBusAxi_ar_payload_lock   = self.dbus.ar.lock,
+            o_dBusAxi_ar_payload_cache  = self.dbus.ar.cache,
+            o_dBusAxi_ar_payload_qos    = self.dbus.ar.qos,
+            o_dBusAxi_ar_payload_prot   = self.dbus.ar.prot,
+            
+            # R.
+            i_dBusAxi_r_valid           = self.dbus.r.valid,
+            o_dBusAxi_r_ready           = self.dbus.r.ready,
+            i_dBusAxi_r_payload_data    = self.dbus.r.data,
+            i_dBusAxi_r_payload_id      = self.dbus.r.id,
+            i_dBusAxi_r_payload_resp    = self.dbus.r.resp,
+            i_dBusAxi_r_payload_last    = self.dbus.r.last,
+
+            # JTAG
+            i_jtag_tms= self.jtag_tms,
+            i_jtag_tdi= self.jtag_tdi,
+            o_jtag_tdo= self.jtag_tdo,
+            i_jtag_tck= self.jtag_tck,
+        )
+
+    def set_reset_address(self, reset_address):
+        assert not hasattr(self, "reset_address")
+        self.reset_address = reset_address
+
+    @staticmethod
+    def add_sources(platform, variant="standard"):
+        rtl_dir = os.path.join(os.path.dirname(__file__), "../src")
+        platform.add_source(os.path.join(rtl_dir, "vexriscv.v"))
+
+    def do_finalize(self):
+        self.specials += Instance("vexriscv", **self.cpu_params)
---
+++ rapidsilicon/ip/vexriscv_cpu/v1_0/vexriscv_cpu_gen.py
@@ -0,0 +1,227 @@
+#!/usr/bin/env python3
+#
+# This file is Copyright (c) 2022 RapidSilicon.
+#
+# SPDX-License-Identifier: MIT.
+
+import os
+import json
+import argparse
+import shutil
+
+from litex_sim.vexriscv_cpu_litex_wrapper import VexRiscv
+
+from migen import *
+
+from litex.build.generic_platform import *
+
+from litex.build.osfpga import OSFPGAPlatform
+
+from litex.soc.interconnect.axi import AXIInterface
+
+
+# IOs / Interface ----------------------------------------------------------------------------------
+def get_clkin_ios():
+    return [
+        ("clk", 0, Pins(1)),
+        ("rst", 0, Pins(1))]
+
+def get_jtag_ios():
+    return [
+                
+            ("jtag_tms",    0,  Pins(1)),
+            ("jtag_tdi",    0,  Pins(1)),
+            ("jtag_tdo",    0,  Pins(1)),
+            ("jtag_tck",    0,  Pins(1)),    
+            ]
+
+def get_other_ios():
+    return [
+            ("timerInterrupt",      0,  Pins(1)),
+            ("externalInterrupt",   0,  Pins(1)),    
+            ("softwareInterrupt",   0,  Pins(1)),    
+            ("debugReset",          0,  Pins(1)),
+            ("debug_resetOut",      0,  Pins(1)),
+        ]
+
+# AXI-VEXRISCV Wrapper --------------------------------------------------------------------------------
+class VexriscvWrapper(Module):
+    def __init__(self, platform):
+        
+        # Clocking
+        platform.add_extension(get_clkin_ios())
+        self.clock_domains.cd_sys = ClockDomain()
+        self.comb += self.cd_sys.clk.eq(platform.request("clk"))
+        self.comb += self.cd_sys.rst.eq(platform.request("rst"))
+
+        #IBUS
+        ibus_axi = AXIInterface(data_width = 32, address_width = 32, id_width = 8)
+        platform.add_extension(ibus_axi.get_ios("ibus_axi"))
+        self.comb += ibus_axi.connect_to_pads(platform.request("ibus_axi"), mode="master")
+
+        #IBUS
+        dbus_axi = AXIInterface(data_width = 32, address_width = 32, id_width = 8)
+        platform.add_extension(dbus_axi.get_ios("dbus_axi"))
+        self.comb += dbus_axi.connect_to_pads(platform.request("dbus_axi"), mode="master")
+
+        # VEXRISCV
+        self.submodules.vexriscv = vexriscv = VexRiscv(platform,
+            ibus        = ibus_axi,
+            dbus        = dbus_axi
+            )
+
+        platform.add_extension(get_jtag_ios())
+        # Inputs
+        self.comb += vexriscv.jtag_tms.eq(platform.request("jtag_tms"))
+        self.comb += vexriscv.jtag_tdi.eq(platform.request("jtag_tdi"))
+        self.comb += vexriscv.jtag_tck.eq(platform.request("jtag_tck"))
+
+        # Outputs
+        self.comb += platform.request("jtag_tdo").eq(vexriscv.jtag_tdo)
+
+        platform.add_extension(get_other_ios())
+        # Inputs
+        self.comb += vexriscv.timerInterrupt.eq(platform.request("timerInterrupt"))
+        self.comb += vexriscv.externalInterrupt.eq(platform.request("externalInterrupt"))
+        self.comb += vexriscv.softwareInterrupt.eq(platform.request("softwareInterrupt"))
+        self.comb += vexriscv.debugReset.eq(platform.request("debugReset"))
+
+        # Outputs
+        self.comb += platform.request("debug_resetOut").eq(vexriscv.debug_resetOut)
+
+# Build --------------------------------------------------------------------------------------------
+def main():
+    parser = argparse.ArgumentParser(description="Vexriscv CORE")
+    parser.formatter_class = lambda prog: argparse.ArgumentDefaultsHelpFormatter(prog,
+        max_help_position = 10,
+        width             = 120
+    )
+
+    # Build Parameters.
+    build_group = parser.add_argument_group(title="Build parameters")
+    build_group.add_argument("--build",         action="store_true",            help="Build Core")
+    build_group.add_argument("--build-dir",     default="./",                   help="Build Directory")
+    build_group.add_argument("--build-name",    default="vexriscv_wrapper",     help="Build Folder Name, Build RTL File Name and Module Name")
+
+    # JSON Import/Template
+    json_group = parser.add_argument_group(title="JSON Parameters")
+    json_group.add_argument("--json",                                    help="Generate Core from JSON File")
+    json_group.add_argument("--json-template",  action="store_true",     help="Generate JSON Template")
+
+    args = parser.parse_args()
+    
+    # Import JSON (Optional) -----------------------------------------------------------------------
+    if args.json:
+        with open(args.json, 'rt') as f:
+            t_args = argparse.Namespace()
+            t_args.__dict__.update(json.load(f))
+            args = parser.parse_args(namespace=t_args)
+
+    # Export JSON Template (Optional) --------------------------------------------------------------
+    if args.json_template:
+        print(json.dumps(vars(args), indent=4))
+
+    # Remove build extension when specified.
+    args.build_name = os.path.splitext(args.build_name)[0]
+
+    # Build Project Directory ----------------------------------------------------------------------
+    if args.build:
+        # Build Path
+        build_path = os.path.join(args.build_dir, 'rapidsilicon/ip/vexriscv_cpu/v1_0/' + (args.build_name))
+        gen_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "vexriscv_cpu_gen.py"))
+        
+        if not os.path.exists(build_path):
+            os.makedirs(build_path)
+            shutil.copy(gen_path, build_path)
+
+        # Litex_sim Path
+        litex_sim_path = os.path.join(build_path, "litex_sim")
+        if not os.path.exists(litex_sim_path):    
+            os.makedirs(litex_sim_path)
+
+        # Simulation Path
+        sim_path = os.path.join(build_path, "sim")
+        if not os.path.exists(sim_path):    
+            os.makedirs(sim_path)
+
+        # Source Path
+        src_path = os.path.join(build_path, "src")
+        if not os.path.exists(src_path):    
+            os.makedirs(src_path) 
+
+        # Synthesis Path
+        synth_path = os.path.join(build_path, "synth")
+        if not os.path.exists(synth_path):    
+            os.makedirs(synth_path) 
+
+        # Design Path
+        design_path = os.path.join("../src", (args.build_name + ".v")) 
+
+        # Copy RTL from Source to Destination
+        rtl_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "src")        
+        rtl_files = os.listdir(rtl_path)
+        for file_name in rtl_files:
+            full_file_path = os.path.join(rtl_path, file_name)
+            if os.path.isfile(full_file_path):
+                shutil.copy(full_file_path, src_path)
+
+        # Copy litex_sim Data from Source to Destination  
+        litex_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "litex_sim")      
+        litex_files = os.listdir(litex_path)
+        for file_name in litex_files:
+            full_file_path = os.path.join(litex_path, file_name)
+            if os.path.isfile(full_file_path):
+                shutil.copy(full_file_path, litex_sim_path)
+        
+        # TCL File Content        
+        tcl = []
+        # Create Design.
+        tcl.append(f"create_design {args.build_name}")
+        # Set Device.
+        tcl.append(f"target_device {'GEMINI'}")
+        # Add Include Path.
+        tcl.append(f"add_library_path {'../src'}")
+        # Add Sources.
+#        for f, typ, lib in file_name:
+        tcl.append(f"add_design_file {design_path}")
+        # Set Top Module.
+        tcl.append(f"set_top_module {args.build_name}")
+        # Add Timings Constraints.
+#        tcl.append(f"add_constraint_file {args.build_name}.sdc")
+        # Run.
+        tcl.append("synthesize")
+
+        # Generate .tcl file
+        tcl_path = os.path.join(synth_path, "raptor.tcl")
+        with open(tcl_path, "w") as f:
+            f.write("\n".join(tcl))
+        f.close()
+        
+    # Create LiteX Core ----------------------------------------------------------------------------
+    platform = OSFPGAPlatform(io=[], toolchain="raptor", device="gemini")
+    module = VexriscvWrapper(platform)
+    
+    # Build
+    if args.build:
+        platform.build(module,
+            build_dir    = "litex_build",
+            build_name   = args.build_name,
+            run          = False,
+            regular_comb = False
+        )
+        shutil.copy(f"litex_build/{args.build_name}.v", src_path)
+        shutil.rmtree("litex_build")
+        
+        # TimeScale Addition to Wrapper
+        wrapper = os.path.join(src_path, f'{args.build_name}.v')
+        f = open(wrapper, "r")
+        content = f.readlines()
+        content.insert(13, '// This file is Copyright (c) 2022 RapidSilicon\n//------------------------------------------------------------------------------')
+        content.insert(15, '\n`timescale 1ns / 1ps\n')
+        f = open(wrapper, "w")
+        content = "".join(content)
+        f.write(str(content))
+        f.close()
+
+if __name__ == "__main__":
+    main()
