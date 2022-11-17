ab62c4037e0aee88b207bc6623234fb7bf1cc776
nadeemyaseen-rs <nadeem.yaseen@rapidsilicon.com>
2022-11-17 22:33:12 +0500

    new changes from Raptor Tools

--- .github/install_ubuntu_dependencies_build.sh
+++ .github/install_ubuntu_dependencies_build.sh
@@ -1,5 +1,7 @@
 sudo apt-get update
 sudo apt install -y cmake build-essential
-sudo apt install -y python3 python3-pip
-python3 -m pip install --upgrade pip
-python3 -m pip install pipenv
+#sudo apt install -y python3 python3-pip
+#python3 -m pip install --upgrade pip
+#python3 -m pip install pipenv
+sudo ln -sf /usr/lib/x86_64-linux-gnu/libffi.so.7.1.0 /usr/lib/x86_64-linux-gnu/libffi.so.6
+
