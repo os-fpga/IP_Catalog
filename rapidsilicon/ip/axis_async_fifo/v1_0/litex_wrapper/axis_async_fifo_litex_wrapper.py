4cd1654fb77fcebd3bb6d018cf6d3c852e7db272
Moin ijaz <moin.ijaz@rslpt42.rapidsilicon.local>
2022-11-30 11:01:19 +0500

    minor bug fixed in axis_async_fifo

--- rapidsilicon/ip/axis_async_fifo/v1_0/litex_wrapper/axis_async_fifo_litex_wrapper.py
+++ rapidsilicon/ip/axis_async_fifo/v1_0/litex_wrapper/axis_async_fifo_litex_wrapper.py
@@ -95,7 +95,7 @@ class AXISASYNCFIFO(Module):
             p_ID_WIDTH              = Instance.PreformattedParam(id_width),
             p_DEST_WIDTH            = Instance.PreformattedParam(dest_width),
             p_USER_WIDTH            = Instance.PreformattedParam(user_width),
-            p_RAM_PIPELINE          = Instance.PreformattedParam(pip_out),
+            p_RAM_PIPELINE          = pip_out,
             p_KEEP_ENABLE           = data_width>8,
             p_LAST_ENABLE           = last_en,
             p_ID_ENABLE             = id_en,
