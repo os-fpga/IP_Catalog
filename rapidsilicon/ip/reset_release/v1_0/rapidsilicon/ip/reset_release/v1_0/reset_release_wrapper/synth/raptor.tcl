0082cacf825c6033d0228f6d79c6307d5fed8091
Bilal Ahmed <bilal.ahmed@rslpt44.rapidsilicon.local>
2022-12-13 16:38:10 +0500

    Remove generated IP

--- rapidsilicon/ip/reset_release/v1_0/rapidsilicon/ip/reset_release/v1_0/reset_release_wrapper/synth/raptor.tcl
+++
@@ -1,8 +0,0 @@
-create_design reset_release_wrapper
-target_device GEMINI
-add_include_path ../src
-add_library_path ../src
-add_library_ext .v .sv
-add_design_file ../src/reset_release_wrapper.v
-set_top_module reset_release_wrapper
-synthesize
