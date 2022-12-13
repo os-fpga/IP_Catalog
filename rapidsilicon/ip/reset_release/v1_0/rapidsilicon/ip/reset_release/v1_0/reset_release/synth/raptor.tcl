f457f4cbc3ebb9cc2bc70f2eb7b2ced26f7e9d57
Bilal Ahmed <bilal.ahmed@rslpt44.rapidsilicon.local>
2022-12-13 16:14:54 +0500

    reset_release IP added

---
+++ rapidsilicon/ip/reset_release/v1_0/rapidsilicon/ip/reset_release/v1_0/reset_release/synth/raptor.tcl
@@ -0,0 +1,8 @@
+create_design reset_release
+target_device GEMINI
+add_include_path ../src
+add_library_path ../src
+add_library_ext .v .sv
+add_design_file ../src/reset_release.v
+set_top_module reset_release
+synthesize
