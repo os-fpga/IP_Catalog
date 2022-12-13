e941a5650f73b2fee89aa6b21f202b8e36b273f9
Roman Shah <roman@rslpt23.rapidsilicon.local>
2022-12-13 18:38:08 +0500

    Design files updated

--- rapidsilicon/ip/axi_crossbar/v2_0/src/afifo.v
+++ rapidsilicon/ip/axi_crossbar/v2_0/src/afifo.v
@@ -1,61 +1,59 @@
 `timescale 1 ns / 1 ps
 
 module afifo #(
-		/* Defining depth of fifo */
-		parameter	ADDRSIZE = 3,
-		//
-		// WIDTH is the number of data bits in each entry
-		parameter	DATASIZE  = 32,
-		//
-		/* Total number of syn stages. The number of stages can be greater than 2 but not less than 2 */
-		parameter	SYNC_STAGES    = 2
+	/* Defining depth of fifo */
+	parameter	ADDRSIZE = 3,
+	//
+	// WIDTH is the number of data bits in each entry
+	parameter	DATASIZE  = 32,
+	//
+	/* Total number of syn stages. The number of stages can be greater than 2 but not less than 2 */
+	parameter	SYNC_STAGES    = 2,
+	/* For BRAM set MEM_TYPE=1, while for Distributed memory set MEM_TYPE=0 */
+	parameter MEM_TYPE = 0
 
 	) 
 	(
 	
-		input	wire			      wclk, 
-		input   wire                  wr_reset, 
-		input   wire                  wr,
-		input	wire [DATASIZE-1:0]	  wr_data,
-		output				          wr_full,
+    input   wire                  wclk, 
+    input   wire                  wr_reset, 
+    input   wire                  wr,
+    input   wire [DATASIZE-1:0]   wr_data,
+    output                        wr_full,
+    input   wire                  rclk,
+    input   wire                  rd_reset,
+    input   wire                  rd,
+    output       [DATASIZE-1:0]   rd_data,
+    output  reg                   rd_empty
 
-		input	wire			      rclk,
-		input   wire                  rd_reset,
-		input   wire                  rd,
-		output		 [DATASIZE-1:0]	  rd_data,
-		output	reg			          rd_empty
+    );
 
-	);
-
-        reg	    [ADDRSIZE:0]		  rd_addr;
-        reg	    [ADDRSIZE:0]		  wr_addr;
-        wire    [ADDRSIZE:0]	      rd_wgray;
-        wire    [ADDRSIZE:0]	      wr_rgray;
-        wire	[ADDRSIZE:0]		  next_rd_addr;
-        wire	[ADDRSIZE:0]		  next_wr_addr;
-        reg	    [ADDRSIZE:0]		  rgray, wgray;
-        wire				          ren;
-        wire                          read_empty;
-
-
-
+    reg     [ADDRSIZE:0]          rd_addr;
+    reg     [ADDRSIZE:0]          wr_addr;
+    wire    [ADDRSIZE:0]          rd_wgray;
+    wire    [ADDRSIZE:0]          wr_rgray;
+    wire    [ADDRSIZE:0]          next_rd_addr;
+    wire    [ADDRSIZE:0]          next_wr_addr;
+    reg     [ADDRSIZE:0]          rgray, wgray;
+    wire                          ren;
+    wire                          read_empty;
 
 
     /* increment write pointer when wr signal goes high and fifo is not full */
-	assign	next_wr_addr = wr_addr + 1;
-	always @(posedge wclk or posedge wr_reset)
-	if (wr_reset)
-	begin
-		wr_addr <= 0;
-		wgray   <= 0;
-	end else if (wr && !wr_full)
-	begin
-		wr_addr <= next_wr_addr;
-		/* Binary to Gray code conversion */
-		wgray   <= next_wr_addr ^ (next_wr_addr >> 1);
-	end
-	
-	/* increment read pointer when rd signal goes high and fifo is not empty */
+    assign	next_wr_addr = wr_addr + 1;
+    always @(posedge wclk or posedge wr_reset)
+    if (wr_reset)
+    begin
+        wr_addr <= 0;
+        wgray   <= 0;
+    end else if (wr && !wr_full)
+    begin
+        wr_addr <= next_wr_addr;
+        /* Binary to Gray code conversion */
+        wgray   <= next_wr_addr ^ (next_wr_addr >> 1);
+    end
+    
+    /* increment read pointer when rd signal goes high and fifo is not empty */
     assign	next_rd_addr = rd_addr + 1;
     always @(posedge rclk or posedge rd_reset)
     if (rd_reset)
@@ -74,9 +72,9 @@ module afifo #(
 	assign 	wr_full = (wr_rgray == { ~wgray[ADDRSIZE:ADDRSIZE-1], wgray[ADDRSIZE-2:0] });
 
 	/* this signal goes high if fifo is empty */
-	assign 	read_empty = (rd_wgray == rgray);
+	assign  read_empty = (rd_wgray == rgray);
 		
-	assign     ren = (rd_empty || rd);
+	assign  ren = (rd_empty || rd);
 
         
     always @(posedge rclk or posedge rd_reset)
@@ -100,7 +98,8 @@ module afifo #(
                 );
 	
    dual_port_ram # (.DATASIZE(DATASIZE),
-                    .ADDRSIZE (ADDRSIZE))
+                    .ADDRSIZE (ADDRSIZE),
+                    .MEM_TYPE(MEM_TYPE))
    dual_port_ram(
                  .rdata  (rd_data),
                  .wr_clk (wclk),
@@ -110,6 +109,6 @@ module afifo #(
                  .wdata  (wr_data),
                  .waddr  (wr_addr),
                  .raddr  (rd_addr)
-                 );	
+                 );
 
 endmodule
--- rapidsilicon/ip/axi_crossbar/v2_0/src/axi_cdc.v
+++ rapidsilicon/ip/axi_crossbar/v2_0/src/axi_cdc.v
@@ -11,176 +11,171 @@ module axi_cdc #(
 	 /* Total number of synchronization stages, to handle metastaibility. This value can be greater but minimum value is 2 */
 	parameter	SYNC_STAGES 	= 	4,
 	/* The log value of total number of entries in FIFO */
-	parameter	FIFO_LOG 		= 	5
+	parameter	FIFO_LOG        = 	5,
+	/* For BRAM set MEM_TYPE=1, while for Distributed memory set MEM_TYPE=0 */
+    parameter   MEM_TYPE = 0
 		
 ) 
 (
 
 
-	input	wire							S_AXI_ACLK,
-	input	wire							S_AXI_ARESET,
-	input	wire							M_AXI_ACLK,
-    input    wire                           M_AXI_ARESET,
+    input   wire                            S_AXI_ACLK,
+    input   wire                            S_AXI_ARESET,
+    input   wire                            M_AXI_ACLK,
+    input   wire                            M_AXI_ARESET,
     `ifdef AXI4FULL
-	/* Address write channel */
-	input	wire [AXI_ID_WIDTH-1 : 0]		S_AXI_AWID,
-	input	wire [AXI_ADDR_WIDTH-1 : 0]		S_AXI_AWADDR,
-	input	wire [7 : 0]					S_AXI_AWLEN,
-	input	wire [2 : 0]					S_AXI_AWSIZE,
-	input	wire [1 : 0]					S_AXI_AWBURST,
-	input	wire [1 : 0]					S_AXI_AWLOCK,
-	input	wire [3 : 0]					S_AXI_AWCACHE,
-	input	wire [2 : 0]					S_AXI_AWPROT,
-	input	wire [3 : 0]					S_AXI_AWQOS,
-	input	wire							S_AXI_AWVALID,
-	output	wire							S_AXI_AWREADY,
-	/* Data write channel */
-	input	wire [AXI_DATA_WIDTH-1 : 0]		S_AXI_WDATA,
-	input	wire [(AXI_DATA_WIDTH/8)-1 : 0] S_AXI_WSTRB,
-	input	wire							S_AXI_WLAST,
-	input	wire							S_AXI_WVALID,
-	output	wire							S_AXI_WREADY,
-	/* Write response channel */	
-	output	wire [AXI_ID_WIDTH-1 : 0]		S_AXI_BID,
-	output	wire [1 : 0]					S_AXI_BRESP,
-	output	wire							S_AXI_BVALID,
-	input	wire							S_AXI_BREADY,
-	/* Address read channel */
-	input	wire [AXI_ID_WIDTH-1 : 0]		S_AXI_ARID,
-	input	wire [AXI_ADDR_WIDTH-1 : 0]		S_AXI_ARADDR,
-	input	wire [7 : 0]					S_AXI_ARLEN,
-	input	wire [2 : 0]					S_AXI_ARSIZE,
-	input	wire [1 : 0]					S_AXI_ARBURST,
-	input	wire [1 : 0]					S_AXI_ARLOCK,
-	input	wire [3 : 0]					S_AXI_ARCACHE,
-	input	wire [2 : 0]					S_AXI_ARPROT,
-	input	wire [3 : 0]					S_AXI_ARQOS,
-	input	wire							S_AXI_ARVALID,
-	output	wire							S_AXI_ARREADY,
-	/* Read response channel */	
-	output	wire [AXI_ID_WIDTH-1 : 0]		S_AXI_RID,
-	output	wire [AXI_DATA_WIDTH-1 : 0]		S_AXI_RDATA,
-	output	wire [1 : 0]					S_AXI_RRESP,
-	output	wire							S_AXI_RLAST,
-	output	wire							S_AXI_RVALID,
-	input	wire							S_AXI_RREADY,
+    /* Address write channel */
+    input   wire [AXI_ID_WIDTH-1 : 0]       S_AXI_AWID,
+    input   wire [AXI_ADDR_WIDTH-1 : 0]     S_AXI_AWADDR,
+    input   wire [7 : 0]                    S_AXI_AWLEN,
+    input   wire [2 : 0]                    S_AXI_AWSIZE,
+    input   wire [1 : 0]                    S_AXI_AWBURST,
+    input   wire [1 : 0]                    S_AXI_AWLOCK,
+    input   wire [3 : 0]                    S_AXI_AWCACHE,
+    input   wire [2 : 0]                    S_AXI_AWPROT,
+    input   wire [3 : 0]                    S_AXI_AWQOS,
+    input   wire                            S_AXI_AWVALID,
+    output  wire                            S_AXI_AWREADY,
+    /* Data write channel */
+    input   wire [AXI_DATA_WIDTH-1 : 0]     S_AXI_WDATA,
+    input   wire [(AXI_DATA_WIDTH/8)-1 : 0] S_AXI_WSTRB,
+    input   wire                            S_AXI_WLAST,
+    input   wire                            S_AXI_WVALID,
+    output  wire                            S_AXI_WREADY,
+    /* Write response channel */	
+    output  wire [AXI_ID_WIDTH-1 : 0]       S_AXI_BID,
+    output  wire [1 : 0]                    S_AXI_BRESP,
+    output  wire                            S_AXI_BVALID,
+    input   wire                            S_AXI_BREADY,
+    /* Address read channel */
+    input	wire [AXI_ID_WIDTH-1 : 0]		S_AXI_ARID,
+    input	wire [AXI_ADDR_WIDTH-1 : 0]		S_AXI_ARADDR,
+    input	wire [7 : 0]                    S_AXI_ARLEN,
+    input	wire [2 : 0]                    S_AXI_ARSIZE,
+    input	wire [1 : 0]                    S_AXI_ARBURST,
+    input	wire [1 : 0]                    S_AXI_ARLOCK,
+    input	wire [3 : 0]                    S_AXI_ARCACHE,
+    input	wire [2 : 0]                    S_AXI_ARPROT,
+    input	wire [3 : 0]                    S_AXI_ARQOS,
+    input   wire                            S_AXI_ARVALID,
+    output  wire                            S_AXI_ARREADY,
+    /* Read response channe   */                 
+    output  wire [AXI_ID_WIDTH-1 : 0]       S_AXI_RID,
+    output  wire [AXI_DATA_WIDTH-1 : 0]     S_AXI_RDATA,
+    output  wire [1 : 0]                    S_AXI_RRESP,
+    output  wire                            S_AXI_RLAST,
+    output  wire                            S_AXI_RVALID,
+    input   wire                            S_AXI_RREADY,
 
 	
 
-	/* Address write channel */ 
-	output	wire [AXI_ID_WIDTH-1 : 0]		M_AXI_AWID,
-	output	wire [AXI_ADDR_WIDTH-1 : 0]		M_AXI_AWADDR,
-	output	wire [7 : 0]					M_AXI_AWLEN,
-	output	wire [2 : 0]					M_AXI_AWSIZE,
-	output	wire [1 : 0]					M_AXI_AWBURST,
-	output	wire [1 : 0]				    M_AXI_AWLOCK,
-	output	wire [3 : 0]					M_AXI_AWCACHE,
-	output	wire [2 : 0]					M_AXI_AWPROT,
-	output	wire [3 : 0]					M_AXI_AWQOS,
-	output	wire							M_AXI_AWVALID,
-	input	wire							M_AXI_AWREADY,
-	/* Data write channel */
-	output	wire [AXI_DATA_WIDTH-1 : 0]		M_AXI_WDATA,
-	output	wire [(AXI_DATA_WIDTH/8)-1 : 0] M_AXI_WSTRB,
-	output	wire							M_AXI_WLAST,
-	output	wire							M_AXI_WVALID,
-	input	wire							M_AXI_WREADY,
-	/* Write response channel */	
-	input	wire [AXI_ID_WIDTH-1 : 0]		M_AXI_BID,
-	input	wire [1 : 0]					M_AXI_BRESP,
-	input	wire							M_AXI_BVALID,
-	output	wire							M_AXI_BREADY,
-	/* Address read channel */
-	output	wire [AXI_ID_WIDTH-1 : 0]		M_AXI_ARID,
-	output	wire [AXI_ADDR_WIDTH-1 : 0]		M_AXI_ARADDR,
-	output	wire [7 : 0]					M_AXI_ARLEN,
-	output	wire [2 : 0]					M_AXI_ARSIZE,
-	output	wire [1 : 0]					M_AXI_ARBURST,
-	output	wire [1 : 0]					M_AXI_ARLOCK,
-	output	wire [3 : 0]					M_AXI_ARCACHE,
-	output	wire [2 : 0]					M_AXI_ARPROT,
-	output	wire [3 : 0]					M_AXI_ARQOS,
-	output	wire							M_AXI_ARVALID,
-	input	wire							M_AXI_ARREADY,
-	/* Read response channel */	
-	input	wire [AXI_ID_WIDTH-1 : 0]		M_AXI_RID,
-	input	wire [AXI_DATA_WIDTH-1 : 0]		M_AXI_RDATA,
-	input	wire [1 : 0]					M_AXI_RRESP,
-	input	wire							M_AXI_RLAST,
-	input	wire							M_AXI_RVALID,
-	output	wire							M_AXI_RREADY,
-	
-	`else
+    /* Address write channel */ 
+    output  wire [AXI_ID_WIDTH-1 : 0]       M_AXI_AWID,
+    output  wire [AXI_ADDR_WIDTH-1 : 0]     M_AXI_AWADDR,
+    output  wire [7 : 0]                    M_AXI_AWLEN,
+    output  wire [2 : 0]                    M_AXI_AWSIZE,
+    output  wire [1 : 0]                    M_AXI_AWBURST,
+    output  wire [1 : 0]                    M_AXI_AWLOCK,
+    output  wire [3 : 0]                    M_AXI_AWCACHE,
+    output  wire [2 : 0]                    M_AXI_AWPROT,
+    output  wire [3 : 0]                    M_AXI_AWQOS,
+    output  wire                            M_AXI_AWVALID,
+    input   wire                            M_AXI_AWREADY,
+    /* Data write channel */
+    output  wire [AXI_DATA_WIDTH-1 : 0]     M_AXI_WDATA,
+    output  wire [(AXI_DATA_WIDTH/8)-1 : 0] M_AXI_WSTRB,
+    output  wire                            M_AXI_WLAST,
+    output  wire                            M_AXI_WVALID,
+    input   wire                            M_AXI_WREADY,
+    /* Write response channel */
+    input   wire [AXI_ID_WIDTH-1 : 0]       M_AXI_BID,
+    input   wire [1 : 0]                    M_AXI_BRESP,
+    input   wire                            M_AXI_BVALID,
+    output  wire                            M_AXI_BREADY,
+    /* Address read channel */
+    output  wire [AXI_ID_WIDTH-1 : 0]       M_AXI_ARID,
+    output  wire [AXI_ADDR_WIDTH-1 : 0]     M_AXI_ARADDR,
+    output  wire [7 : 0]                    M_AXI_ARLEN,
+    output  wire [2 : 0]                    M_AXI_ARSIZE,
+    output  wire [1 : 0]                    M_AXI_ARBURST,
+    output  wire [1 : 0]                    M_AXI_ARLOCK,
+    output  wire [3 : 0]                    M_AXI_ARCACHE,
+    output  wire [2 : 0]                    M_AXI_ARPROT,
+    output  wire [3 : 0]                    M_AXI_ARQOS,
+    output  wire                            M_AXI_ARVALID,
+    input   wire                            M_AXI_ARREADY,
+    /* Read response channel */	
+    input   wire [AXI_ID_WIDTH-1 : 0]       M_AXI_RID,
+    input   wire [AXI_DATA_WIDTH-1 : 0]     M_AXI_RDATA,
+    input   wire [1 : 0]                    M_AXI_RRESP,
+    input   wire                            M_AXI_RLAST,
+    input   wire                            M_AXI_RVALID,
+    output  wire                            M_AXI_RREADY,   
+    `else
     /*  AXI4 LITE slave interface */
     /* Address read channel */
     input    wire [AXI_ADDR_WIDTH-1 : 0]     S_AXI_ARADDR,
     input    wire [2 : 0]                    S_AXI_ARPROT,
     output   wire                            S_AXI_ARREADY,
     input    wire                            S_AXI_ARVALID,
-    
     /* Address write channel */
     input    wire [AXI_ADDR_WIDTH-1 : 0]     S_AXI_AWADDR,
     input    wire [2 : 0]                    S_AXI_AWPROT,
     output   wire                            S_AXI_AWREADY,
     input    wire                            S_AXI_AWVALID,
-       
-     /* Write response channel */
-     input    wire                            S_AXI_BREADY,
-     output   wire [1 : 0]                    S_AXI_BRESP,
-     output   wire                            S_AXI_BVALID,
-       
-     /* Read response channel */
-     output   wire [AXI_DATA_WIDTH-1 : 0]     S_AXI_RDATA,
-     input    wire                            S_AXI_RREADY,
-     output   wire [1 : 0]                    S_AXI_RRESP,
-     output   wire                            S_AXI_RVALID,
-         
-     /* Data write channel */
-     input    wire [AXI_DATA_WIDTH-1 : 0]     S_AXI_WDATA,
-     output   wire                            S_AXI_WREADY,
-     input    wire [(AXI_DATA_WIDTH/8)-1 : 0] S_AXI_WSTRB,
-     input    wire                            S_AXI_WVALID,
+      
+    /* Write response channel */
+    input    wire                            S_AXI_BREADY,
+    output   wire [1 : 0]                    S_AXI_BRESP,
+    output   wire                            S_AXI_BVALID,
+      
+    /* Read response channel */
+    output   wire [AXI_DATA_WIDTH-1 : 0]     S_AXI_RDATA,
+    input    wire                            S_AXI_RREADY,
+    output   wire [1 : 0]                    S_AXI_RRESP,
+    output   wire                            S_AXI_RVALID,
+        
+    /* Data write channel */
+    input    wire [AXI_DATA_WIDTH-1 : 0]     S_AXI_WDATA,
+    output   wire                            S_AXI_WREADY,
+    input    wire [(AXI_DATA_WIDTH/8)-1 : 0] S_AXI_WSTRB,
+    input    wire                            S_AXI_WVALID,
     
     
      /*  AXI4 LITE  master interface  */
     
      /* Address write channel */ 
-     output   wire [AXI_ADDR_WIDTH-1 : 0]     M_AXI_AWADDR,
-     output   wire [2 : 0]                    M_AXI_AWPROT,
-     output   wire                            M_AXI_AWVALID,
-     input    wire                            M_AXI_AWREADY,
-    
-     /* Data write channel */
-     output   wire [AXI_DATA_WIDTH-1 : 0]     M_AXI_WDATA,
-     output   wire [(AXI_DATA_WIDTH/8)-1 : 0] M_AXI_WSTRB,
-     output   wire                            M_AXI_WVALID,
-     input    wire                            M_AXI_WREADY,
-    
-     /* Write response channel */
-     input    wire [1 : 0]                    M_AXI_BRESP,
-     input    wire                            M_AXI_BVALID,
-     output   wire                            M_AXI_BREADY,
-    
-     /* Address read channel */
-     output   wire [AXI_ADDR_WIDTH-1 : 0]     M_AXI_ARADDR,
-     output   wire [2 : 0]                    M_AXI_ARPROT,
-     output   wire                            M_AXI_ARVALID,
-     input    wire                            M_AXI_ARREADY,
-    
-     /* Read response channel */
-     input    wire [AXI_DATA_WIDTH-1 : 0]     M_AXI_RDATA,
-     input    wire [1 : 0]                    M_AXI_RRESP,
-     input    wire                            M_AXI_RVALID,
-     output   wire                            M_AXI_RREADY,
-    
-     `endif AXI4FULL	
-	
-	
-	
-		
+    output   wire [AXI_ADDR_WIDTH-1 : 0]     M_AXI_AWADDR,
+    output   wire [2 : 0]                    M_AXI_AWPROT,
+    output   wire                            M_AXI_AWVALID,
+    input    wire                            M_AXI_AWREADY,
+    /* Data write channel */
+    output   wire [AXI_DATA_WIDTH-1 : 0]     M_AXI_WDATA,
+    output   wire [(AXI_DATA_WIDTH/8)-1 : 0] M_AXI_WSTRB,
+    output   wire                            M_AXI_WVALID,
+    input    wire                            M_AXI_WREADY,
+    /* Write response channel */
+    input    wire [1 : 0]                    M_AXI_BRESP,
+    input    wire                            M_AXI_BVALID,
+    output   wire                            M_AXI_BREADY,
+    /* Address read channel */
+    output   wire [AXI_ADDR_WIDTH-1 : 0]     M_AXI_ARADDR,
+    output   wire [2 : 0]                    M_AXI_ARPROT,
+    output   wire                            M_AXI_ARVALID,
+    input    wire                            M_AXI_ARREADY,
+    /* Read response channel */
+    input    wire [AXI_DATA_WIDTH-1 : 0]     M_AXI_RDATA,
+    input    wire [1 : 0]                    M_AXI_RRESP,
+    input    wire                            M_AXI_RVALID,
+    output   wire                            M_AXI_RREADY,
+
+    `endif AXI4FULL	
+
+
+
 );
 
-    wire	awfull, awempty, wfull, wempty, bfull, bempty,arfull, arempty, rfull, rempty;
+    wire  awfull, awempty, wfull, wempty, bfull, bempty,arfull, arempty, rfull, rempty;
     
     `ifdef AXI4FULL
     
@@ -206,8 +201,8 @@ module axi_cdc #(
         .rd_empty(awempty)
     );
     
-    assign	M_AXI_AWVALID = !awempty;
-    assign	S_AXI_AWREADY = !awfull;
+    assign M_AXI_AWVALID = !awempty;
+    assign S_AXI_AWREADY = !awfull;
     
     afifo #(
     .ADDRSIZE (FIFO_LOG),
@@ -410,6 +405,9 @@ module axi_cdc #(
         .rd_data({S_AXI_RDATA, S_AXI_RRESP}),
         .rd_empty(rempty)
         );
+
+    assign	S_AXI_RVALID = !rempty;
+    assign	M_AXI_RREADY = !rfull;
         
        `endif
 
