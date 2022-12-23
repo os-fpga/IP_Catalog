17d27b3e75da352910a8e66c3889c7d3fbe0f692
Moin ijaz <moin.ijaz@rslpt42.rapidsilicon.local>
2022-12-23 14:46:10 +0500

    version support added

--- rapidsilicon/ip/axis_ram_switch/v1_0/axis_ram_switch_gen.py
+++ rapidsilicon/ip/axis_ram_switch/v1_0/axis_ram_switch_gen.py
@@ -277,8 +277,10 @@ def main():
     if args.build:
         rs_builder.prepare(
             build_dir  = args.build_dir,
-            build_name = args.build_name
+            build_name = args.build_name,
+            version    = "v1_0"
         )
+        
         rs_builder.copy_files(gen_path=os.path.dirname(__file__))
         rs_builder.generate_tcl()
         rs_builder.generate_wrapper(
