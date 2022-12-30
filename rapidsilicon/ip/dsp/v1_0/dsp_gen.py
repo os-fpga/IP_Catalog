43483bec04ffdea72cb519bc1201d433c8a7ad80
Moin ijaz <moin.ijaz@rslpt42.rapidsilicon.local>
2022-12-30 12:35:11 +0500

    dsp updated

--- rapidsilicon/ip/dsp/v1_0/dsp_gen.py
+++ rapidsilicon/ip/dsp/v1_0/dsp_gen.py
@@ -39,30 +39,22 @@ def get_ios(a_width, b_width, c_width, d_width, e_width, f_width, g_width, h_wid
     ]
 
 class RS_DSP_Wrapper(Module):
-    def __init__(self, platform, a_width, b_width, c_width, d_width, e_width, f_width, g_width, h_width, feature, reg_in, reg_out):
+    def __init__(self, platform, a_width, b_width, c_width, d_width, e_width, f_width, g_width, h_width, feature, reg_in, reg_out, unsigned_a, unsigned_b, unsigned_c, unsigned_d, unsigned_e, unsigned_f, unsigned_g, unsigned_h):
     
     # Clocking
         self.clock_domains.cd_sys = ClockDomain()
         platform.add_extension(get_clkin_ios())
-
-        # Clock/Reset
-        if (reg_in == 1 or reg_out == 1):
-            self.comb += self.cd_sys.clk.eq(platform.request("clk"))
-            self.comb += self.cd_sys.rst.eq(platform.request("reset"))
-        
-        if (a_width > b_width):
-            k = int(a_width/2)
-        else:
-            k = int(b_width/2)
         
         # A*B
         if (feature == "A*B"):
             if (a_width > 20 or b_width > 18):
-                z_width = 38+1+2*(k)
-                self.submodules.dsp = dsp = RS_DSP_MULT20(a_width, b_width, feature, reg_in, reg_out)
+                z_width = a_width + b_width
+                self.submodules.dsp = dsp = RS_DSP_MULT20(a_width, b_width, feature, reg_in, reg_out, unsigned_a, unsigned_b)
                 platform.add_extension(get_ios(a_width, b_width, c_width, d_width, e_width, f_width, g_width, h_width, z_width))
                 self.comb += dsp.a.eq(platform.request("a"))
                 self.comb += dsp.b.eq(platform.request("b"))
+                self.comb += self.cd_sys.clk.eq(platform.request("clk"))
+                self.comb += self.cd_sys.rst.eq(platform.request("reset"))
                 
                 # Registered Output
                 if (reg_out == 1):
@@ -71,17 +63,21 @@ class RS_DSP_Wrapper(Module):
                     self.comb += platform.request("z").eq(dsp.z)
                 
             else:
-                z_width = 38
-                self.submodules.dsp = dsp = RS_DSP_MULT(a_width, b_width, feature, reg_in, reg_out)
+                z_width = a_width + b_width
+                self.submodules.dsp = dsp = RS_DSP_MULT(a_width, b_width, feature, reg_in, reg_out, unsigned_a, unsigned_b)
                 platform.add_extension(get_ios(a_width, b_width, c_width, d_width, e_width, f_width, g_width, h_width, z_width))
                 self.comb += dsp.a.eq(platform.request("a"))
                 self.comb += dsp.b.eq(platform.request("b"))
                 self.comb += platform.request("z").eq(dsp.z)
+                # Clock/Reset
+                if (reg_in == 1 or reg_out == 1):
+                    self.comb += self.cd_sys.clk.eq(platform.request("clk"))
+                    self.comb += self.cd_sys.rst.eq(platform.request("reset"))
         
         # (A*B)+(C*D)
         if (feature=="A*B+C*D"):
             z_width = 39
-            self.submodules.dsp = dsp = RS_DSP_MULT_ABCD(a_width, b_width, c_width, d_width, feature, reg_in, reg_out)
+            self.submodules.dsp = dsp = RS_DSP_MULT_ABCD(a_width, b_width, c_width, d_width, feature, reg_in, reg_out, unsigned_a, unsigned_b, unsigned_c, unsigned_d)
             platform.add_extension(get_ios(a_width, b_width, c_width, d_width, e_width, f_width, g_width, h_width, z_width))
             self.comb += dsp.a.eq(platform.request("a"))
             self.comb += dsp.b.eq(platform.request("b"))
@@ -92,11 +88,15 @@ class RS_DSP_Wrapper(Module):
                 self.sync += platform.request("z").eq(dsp.z)
             else:
                 self.comb += platform.request("z").eq(dsp.z)
+            # Clock/Reset
+            if (reg_in == 1 or reg_out == 1):
+                self.comb += self.cd_sys.clk.eq(platform.request("clk"))
+                self.comb += self.cd_sys.rst.eq(platform.request("reset"))
         
         # A*B+C*D+E*F+G*H
         elif (feature=="A*B+C*D+E*F+G*H"):
             z_width = 40
-            self.submodules.dsp = dsp = RS_DSP_MULT_ABCDEFGH(a_width, b_width, c_width, d_width, e_width, f_width, g_width, h_width, feature, reg_in, reg_out)
+            self.submodules.dsp = dsp = RS_DSP_MULT_ABCDEFGH(a_width, b_width, c_width, d_width, e_width, f_width, g_width, h_width, feature, reg_in, reg_out, unsigned_a, unsigned_b, unsigned_c, unsigned_d, unsigned_e, unsigned_f, unsigned_g, unsigned_h)
             platform.add_extension(get_ios(a_width, b_width, c_width, d_width, e_width, f_width, g_width, h_width, z_width))
             self.comb += dsp.a.eq(platform.request("a"))
             self.comb += dsp.b.eq(platform.request("b"))
@@ -107,6 +107,10 @@ class RS_DSP_Wrapper(Module):
             self.comb += dsp.g.eq(platform.request("g"))
             self.comb += dsp.h.eq(platform.request("h"))
             self.comb += platform.request("z").eq(dsp.z)
+            # Clock/Reset
+            if (reg_in == 1 or reg_out == 1):
+                self.comb += self.cd_sys.clk.eq(platform.request("clk"))
+                self.comb += self.cd_sys.rst.eq(platform.request("reset"))
 
 def main():
     # DSP CORE -------------------------------------------------------------------------------------
@@ -124,25 +128,33 @@ def main():
     # IP Builder.
     rs_builder = IP_Builder(device="gemini", ip_name="dsp", language="verilog")
     
+    # Core string parameters.
+    core_string_param_group = parser.add_argument_group(title="Core string parameters")
+    core_string_param_group.add_argument("--feature",     type=str,      default="A*B",      choices=["A*B","A*B+C*D","A*B+C*D+E*F+G*H"],    help="Features")
+    
     # Core range value parameters.
     core_range_param_group = parser.add_argument_group(title="Core range parameters")
-    core_range_param_group.add_argument("--a_width",     type=int,       default=20,      choices=range(1, 37),                           help="A_Input")
-    core_range_param_group.add_argument("--b_width",     type=int,       default=18,      choices=range(1, 37),                           help="B_Input")
-    core_range_param_group.add_argument("--c_width",     type=int,       default=20,      choices=range(1, 21),                           help="C_Input")
-    core_range_param_group.add_argument("--d_width",     type=int,       default=18,      choices=range(1, 19),                           help="D_Input")
-    core_range_param_group.add_argument("--e_width",     type=int,       default=20,      choices=range(1, 21),                           help="E_Input")
-    core_range_param_group.add_argument("--f_width",     type=int,       default=18,      choices=range(1, 19),                           help="F_Input")
-    core_range_param_group.add_argument("--g_width",     type=int,       default=20,      choices=range(1, 21),                           help="G_Input")
-    core_range_param_group.add_argument("--h_width",     type=int,       default=18,      choices=range(1, 19),                           help="H_Input")
-
-    # Core string value parameters.
-    core_string_param_group = parser.add_argument_group(title="Core fix parameters")
-    core_string_param_group.add_argument("--feature",     type=str,      default="A*B",      choices=["A*B","A*B+C*D","A*B+C*D+E*F+G*H"],    help="Features")
+    core_range_param_group.add_argument("--a_width",     type=int,       default=20,      choices=range(1, 37),     help="A_Input")
+    core_range_param_group.add_argument("--b_width",     type=int,       default=18,      choices=range(1, 37),     help="B_Input")
+    core_range_param_group.add_argument("--c_width",     type=int,       default=20,      choices=range(1, 21),     help="C_Input")
+    core_range_param_group.add_argument("--d_width",     type=int,       default=18,      choices=range(1, 19),     help="D_Input")
+    core_range_param_group.add_argument("--e_width",     type=int,       default=20,      choices=range(1, 21),     help="E_Input")
+    core_range_param_group.add_argument("--f_width",     type=int,       default=18,      choices=range(1, 19),     help="F_Input")
+    core_range_param_group.add_argument("--g_width",     type=int,       default=20,      choices=range(1, 21),     help="G_Input")
+    core_range_param_group.add_argument("--h_width",     type=int,       default=18,      choices=range(1, 19),     help="H_Input")
     
     # Core bool value parameters.
     core_bool_param_group = parser.add_argument_group(title="Core bool parameters")
-    core_bool_param_group.add_argument("--reg_in",  type=bool,    default=False,    help="Registered Inputs")
-    core_bool_param_group.add_argument("--reg_out", type=bool,    default=False,    help="Registered Outputs")
+    core_bool_param_group.add_argument("--reg_in",      type=bool,    default=False,    help="Registered Inputs")
+    core_bool_param_group.add_argument("--reg_out",     type=bool,    default=False,    help="Registered Outputs")
+    core_bool_param_group.add_argument("--unsigned_a",  type=bool,    default=False,    help="Unsigned Input A")
+    core_bool_param_group.add_argument("--unsigned_b",  type=bool,    default=False,    help="Unsigned Input B")
+    core_bool_param_group.add_argument("--unsigned_c",  type=bool,    default=False,    help="Unsigned Input C")
+    core_bool_param_group.add_argument("--unsigned_d",  type=bool,    default=False,    help="Unsigned Input D")
+    core_bool_param_group.add_argument("--unsigned_e",  type=bool,    default=False,    help="Unsigned Input E")
+    core_bool_param_group.add_argument("--unsigned_f",  type=bool,    default=False,    help="Unsigned Input F")
+    core_bool_param_group.add_argument("--unsigned_g",  type=bool,    default=False,    help="Unsigned Input G")
+    core_bool_param_group.add_argument("--unsigned_h",  type=bool,    default=False,    help="Unsigned Input H")
 
     # Build Parameters.
     build_group = parser.add_argument_group(title="Build parameters")
@@ -160,7 +172,7 @@ def main():
     # Import JSON (Optional) -----------------------------------------------------------------------
     if args.json:
         args = rs_builder.import_args_from_json(parser=parser, json_filename=args.json)
-    
+
     # Export JSON Template (Optional) --------------------------------------------------------------
     if args.json_template:
         rs_builder.export_json_template(parser=parser, dep_dict=dep_dict)
@@ -178,7 +190,15 @@ def main():
         h_width     = args.h_width,
         feature     = args.feature,
         reg_in      = args.reg_in,
-        reg_out     = args.reg_out
+        reg_out     = args.reg_out,
+        unsigned_a  = args.unsigned_a,
+        unsigned_b  = args.unsigned_b,
+        unsigned_c  = args.unsigned_c,
+        unsigned_d  = args.unsigned_d,
+        unsigned_e  = args.unsigned_e,
+        unsigned_f  = args.unsigned_f,
+        unsigned_g  = args.unsigned_g,
+        unsigned_h  = args.unsigned_h
     )
     
     # Build Project --------------------------------------------------------------------------------
@@ -188,7 +208,6 @@ def main():
             build_name = args.build_name,
             version    = "v1_0"
         )
-        
         rs_builder.copy_files(gen_path=os.path.dirname(__file__))
         rs_builder.generate_tcl()
         rs_builder.generate_wrapper(
