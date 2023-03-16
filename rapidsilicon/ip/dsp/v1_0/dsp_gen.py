41e56dc330f26bb09c2462b4cfe325104d39044b
Sarmad Salman <ssalman@rslpt09.rapidsilicon.local>
2023-03-16 12:14:58 +0500

    minor fix for UI parameters

--- rapidsilicon/ip/dsp/v1_0/dsp_gen.py
+++ rapidsilicon/ip/dsp/v1_0/dsp_gen.py
@@ -196,6 +196,7 @@ def main():
     if args.json:
         args = rs_builder.import_args_from_json(parser=parser, json_filename=args.json)
         if (args.equation == "A*B"):
+            parser._actions[2].choices = ["Base", "Enhanced", "Pipeline"]
             if (args.feature == "Base") or (args.feature == "Pipeline" and args.unsigned == True):
                 if(args.feature == "Pipeline"):
                     parser._actions[11].default = True
