6879dd72353ff7d5fe7bfc52504bae5a9233c330
Bilal Ahmed <bilal.ahmed@rslpt44.rapidsilicon.local>
2023-03-24 15:27:40 +0500

    Common.py updated with disable feature

--- rapidsilicon/lib/common.py
+++ rapidsilicon/lib/common.py
@@ -100,6 +100,7 @@ class IP_Builder:
                             "default"       : str(core_action.default),
                             "type"          : str("int"),
                             "description"   : str(core_action.help),
+                            "disable"       : str("False"),
                         })
 
             if core_bool_param_group is not None:
@@ -111,6 +112,7 @@ class IP_Builder:
                             "default"       : str(core_action.default),
                             "type"          : str("bool"),
                             "description"   : str(core_action.help),
+                            "disable"       : str("False"),
                         })
             if core_range_param_group is not None:
                 for core_action in core_range_param_group._group_actions:
@@ -123,6 +125,7 @@ class IP_Builder:
                             "type"          : str("int"),
                             "default"       : str(core_action.default),
                             "description"   : str(core_action.help),
+                            "disable"       : str("False"),
                         })
             if core_str_param_group is not None:
                 for core_action in core_str_param_group._group_actions:
@@ -134,6 +137,7 @@ class IP_Builder:
                             "default"       : str(core_action.default),
                             "type"          : str("str"),
                             "description"   : str(core_action.help),
+                            "disable"       : str("False"),
                         })
 
             if core_file_path_group is not None:
@@ -145,6 +149,7 @@ class IP_Builder:
                             "default"       : str(core_action.default),
                             "type"          : str("str"),
                             "description"   : str(core_action.help),
+                            "disable"       : str("False"),
                         })
 
             if name.startswith("build"):
@@ -163,7 +168,9 @@ class IP_Builder:
 
         for i in range(len(core_param_list)):
             if param_json["parameters"][i]['parameter'] in dep_list:
-                param_json["parameters"][i].update(dependency = dep_dict[param_json["parameters"][i]['parameter']])
+                param_json["parameters"][i].update(disable = dep_dict[param_json["parameters"][i]['parameter']])
+
+
 
     # Append Build and Json params to final json
         for i in range(len(build_param_list)):
