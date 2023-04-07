475b8f3077d95b5126eb9df2241e5596d4496b76
Sarmad Salman <ssalman@rslpt09.rapidsilicon.local>
2023-04-07 12:48:54 +0500

    removed redundant files

--- rapidsilicon/ip/axi_dpram/v1_0/sim/sim.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-raptor_exe=`which raptor`
-[ -z "$raptor_exe" ] && { echo "Make sure Raptor bin path is in your PATH"; exit 1; }
-bin_path=`dirname $raptor_exe`
-export PATH=$bin_path/../share/envs/litex/bin:$PATH
-export LIBPYTHON_LOC=$bin_path/../share/envs/python3.8/lib/libpython3.8.so.1.0
-make
