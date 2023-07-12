8c5bb2d74f31a23b916fc4f9af78df607b5b1205
sarmadsalman-RS <sarmad.salman@rapidsilicon.com>
2023-07-12 12:05:04 +0500

    removed redundant files

--- rapidsilicon/ip/dsp_generator/v1_0/rapidsilicon/ip/dsp_generator/v1_0/dsp_generator/synth/raptor.tcl
+++
@@ -1,8 +0,0 @@
-create_design dsp_generator
-target_device GEMINI
-add_include_path ../src
-add_library_path ../src
-add_library_ext .v .sv
-add_design_file ../src/dsp_generator.v
-set_top_module dsp_generator
-synthesize
