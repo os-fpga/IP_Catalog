57c2b357a6fa7f999a870429a846ce9bdc6d8b8a
sarmadsalman-RS <sarmad.salman@rapidsilicon.com>
2023-07-12 11:53:07 +0500

    deleted redundant folder

--- rapidsilicon/ip/fifo_generator/v1_0/rapidsilicon/ip/fifo_generator/v1_0/FIFO_generator/synth/raptor.tcl
+++
@@ -1,8 +0,0 @@
-create_design FIFO_generator
-target_device GEMINI
-add_include_path ../src
-add_library_path ../src
-add_library_ext .v .sv
-add_design_file ../src/FIFO_generator.v
-set_top_module FIFO_generator
-synthesize
