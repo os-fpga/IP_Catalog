32d5e818dbd4d1879764f4869b0fdabc318906dd
sarmadsalman-RS <sarmad.salman@rapidsilicon.com>
2023-09-21 12:29:51 +0500

    updated according to new primitives

--- rapidsilicon/ip/fifo_generator/v1_0/sim/FIFO.v
+++ rapidsilicon/ip/fifo_generator/v1_0/sim/FIFO.v
@@ -1,16 +1,21 @@
-// Copyright (C) 2023 RapidSilicon
+// --------------------------------------------------------------------------
+// ---------------- Copyright (C) 2023 RapidSilicon -------------------------
+// --------------------------------------------------------------------------
+// ---------------------- FIFO Primitive ------------------------------------
+// --------------------------------------------------------------------------
 
 module FIFO #(
     // Parameter Definition
-    parameter   DATA_WIDTH          = 36,
-    parameter   SYNC_FIFO           = "TRUE",
-    parameter   PROG_FULL_THRESH    = 12'b100000000000,
-    parameter   PROG_EMPTY_THRESH   = 12'b111111111100
+    parameter   DATA_WIDTH_WRITE    = 6'd36,
+    parameter   DATA_WIDTH_READ     = 6'd36,
+    parameter   SYNC_FIFO           = "SYNCHRONOUS",
+    parameter   PROG_FULL_THRESH    = 12'b111111111100,
+    parameter   PROG_EMPTY_THRESH   = 12'b000000000000
 )
 (
     // Input/Output
-    input wire  [DATA_WIDTH-1:0] WR_DATA,
-    output wire [DATA_WIDTH-1:0] RD_DATA,
+    input wire  [DATA_WIDTH_WRITE-1:0] WR_DATA,
+    output wire [DATA_WIDTH_READ-1:0] RD_DATA,
     output wire EMPTY,
     output wire FULL,
     output wire OVERFLOW,
@@ -27,35 +32,35 @@ module FIFO #(
 );
 
 // Signals
-wire TEMP_CLK;
+wire second_clk;
 
 // Initial Block
 initial 
 begin
-    if (!(SYNC_FIFO === "TRUE" || SYNC_FIFO === "FALSE")) begin
-        $error("Incorrect SYNC_FIFO Value: '%s'\nEnter valid SYNC_FIFO value: 'TRUE'/'FALSE'", SYNC_FIFO);
+    if (!(SYNC_FIFO === "SYNCHRONOUS" || SYNC_FIFO === "ASYNCHRONOUS")) begin
+        $error("Incorrect SYNC_FIFO Value: '%s'\nEnter valid SYNC_FIFO value: 'SYNCHRONOUS'/'ASYNCHRONOUS'", SYNC_FIFO);
         $finish;
     end
 end
 
 // Common Clock when Synchronous Selected
-assign TEMP_CLK         = (SYNC_FIFO == "TRUE")     ? WRCLK     : RDCLK;
+assign second_clk         = (SYNC_FIFO == "SYNCHRONOUS")     ? WRCLK     : RDCLK;
 
 // Synchronous/Asynchronous FIFO 
-localparam SYNC_FIFO1   = (SYNC_FIFO == "TRUE")     ? 1'b1      : 1'b0;
+localparam sync_fifo   = (SYNC_FIFO == "SYNCHRONOUS")     ? 1'b1      : 1'b0;
 
 // FIFO
 generate
-if (DATA_WIDTH == 36)
+if (DATA_WIDTH_WRITE == 6'd36 && DATA_WIDTH_READ == 6'd36)
     begin
         RS_TDP36K #(
-            .MODE_BITS({SYNC_FIFO1, {4{3'b110}}, 1'b1, 1'b0, 1'b0, 1'b0, PROG_EMPTY_THRESH, PROG_FULL_THRESH, 39'd0, 1'b0})
+            .MODE_BITS({sync_fifo, {4{3'b110}}, 1'b1, 1'b0, 1'b0, 1'b0, PROG_EMPTY_THRESH, PROG_FULL_THRESH, 39'd0, 1'b0})
             )
-        RS_TDP36K_inst1(
+        RS_TDP36K_W36_R36 (
             .WEN_A1(WREN),
             .REN_B1(RDEN),
             .CLK_A1(WRCLK),
-            .CLK_B1(TEMP_CLK),
+            .CLK_B1(second_clk),
             .WDATA_A1(WR_DATA[17:0]),
             .WDATA_A2(WR_DATA[35:18]),
             .RDATA_A1({EMPTY, ALMOST_EMPTY, PROG_EMPTY, UNDERFLOW, FULL, ALMOST_FULL, PROG_FULL, OVERFLOW}),
@@ -63,104 +68,174 @@ if (DATA_WIDTH == 36)
             .RDATA_B2(RD_DATA[35:18]),
             .FLUSH1(RESET),
             .CLK_A2(WRCLK),
-            .CLK_B2(TEMP_CLK)
+            .CLK_B2(second_clk)
         );
     end
 
-else if (DATA_WIDTH == 18)
+else if (DATA_WIDTH_WRITE == 5'd18 && DATA_WIDTH_READ == 5'd18)
     begin
         RS_TDP36K #(
-            .MODE_BITS({SYNC_FIFO1, {4{3'b010}}, 1'b1, 1'b0, 1'b0, 1'b0, PROG_EMPTY_THRESH, PROG_FULL_THRESH, 39'd0, 1'b1})
+            // ----------------------------------------------------------Appending 12th bit as dont care bit
+            .MODE_BITS({sync_fifo, {4{3'b010}}, 1'b1, 1'b0, 1'b0, 1'b0, 1'b0, PROG_EMPTY_THRESH[10:0], 1'b0, PROG_FULL_THRESH[10:0], 39'd0, 1'b1})
             )
-        RS_TDP36K_inst1(
+        RS_TDP36K_W18_R18 (
             .WEN_A1(WREN),
             .REN_B1(RDEN),
             .CLK_A1(WRCLK),
-            .CLK_B1(TEMP_CLK),
+            .CLK_B1(second_clk),
             .WDATA_A1(WR_DATA[17:0]),
             .RDATA_A1({EMPTY, ALMOST_EMPTY, PROG_EMPTY, UNDERFLOW, FULL, ALMOST_FULL, PROG_FULL, OVERFLOW}),
             .RDATA_B1(RD_DATA[17:0]),
             .FLUSH1(RESET),
             .CLK_A2(WRCLK),
-            .CLK_B2(TEMP_CLK)
+            .CLK_B2(second_clk)
         );
     end
 
-else if (DATA_WIDTH == 9)
+else if (DATA_WIDTH_WRITE == 4'd9 && DATA_WIDTH_READ == 4'd9)
     begin
+        wire [17:0] rd_data;
+        assign RD_DATA = {rd_data[16], rd_data[7:0]};
         RS_TDP36K #(
-            .MODE_BITS({SYNC_FIFO1, {4{3'b100}}, 1'b1, 1'b0, 1'b0, 1'b0, PROG_EMPTY_THRESH, PROG_FULL_THRESH, 39'd0, 1'b1})
+            // ----------------------------------------------------------Appending 12th bit as dont care bit
+            .MODE_BITS({sync_fifo, {4{3'b100}}, 1'b1, 1'b0, 1'b0, 1'b0, 1'b0, PROG_EMPTY_THRESH[10:0], 1'b0, PROG_FULL_THRESH[10:0], 39'd0, 1'b1})
             )
-        RS_TDP36K_inst1(
+        RS_TDP36K_W9_R9 (
             .WEN_A1(WREN),
             .REN_B1(RDEN),
             .CLK_A1(WRCLK),
-            .CLK_B1(TEMP_CLK),
-            .WDATA_A1(WR_DATA[8:0]),
+            .CLK_B1(second_clk),
+            .WDATA_A1({1'dx, WR_DATA[8], {8{1'dx}}, WR_DATA[7:0]}),
             .RDATA_A1({EMPTY, ALMOST_EMPTY, PROG_EMPTY, UNDERFLOW, FULL, ALMOST_FULL, PROG_FULL, OVERFLOW}),
-            .RDATA_B1(RD_DATA[8:0]),
+            .RDATA_B1(rd_data),
             .FLUSH1(RESET),
             .CLK_A2(WRCLK),
-            .CLK_B2(TEMP_CLK)
+            .CLK_B2(second_clk)
+        );
+    end
+else if (DATA_WIDTH_WRITE == 6'd36 && DATA_WIDTH_READ == 5'd18)
+    begin
+        RS_TDP36K #(
+            .MODE_BITS({sync_fifo, {2{3'b010}}, {2{3'b110}}, 1'b1, 1'b0, 1'b0, 1'b0, PROG_EMPTY_THRESH, PROG_FULL_THRESH, 39'd0, 1'b0})
+            )
+        RS_TDP36K_W36_R18 (
+            .WEN_A1(WREN),
+            .REN_B1(RDEN),
+            .CLK_A1(WRCLK),
+            .CLK_B1(second_clk),
+            .WDATA_A1(WR_DATA[17:0]),
+            .WDATA_A2(WR_DATA[35:18]),
+            .RDATA_A1({EMPTY, ALMOST_EMPTY, PROG_EMPTY, UNDERFLOW, FULL, ALMOST_FULL, PROG_FULL, OVERFLOW}),
+            .RDATA_B1(RD_DATA[17:0]),
+            .FLUSH1(RESET),
+            .CLK_A2(WRCLK),
+            .CLK_B2(second_clk)
+        );
+    end
+else if (DATA_WIDTH_WRITE == 6'd36 && DATA_WIDTH_READ == 4'd9)
+    begin
+        wire [17:0] rd_data;
+        assign RD_DATA = {rd_data[16], rd_data[7:0]};
+        RS_TDP36K #(
+            .MODE_BITS({sync_fifo, {2{3'b100}}, {2{3'b110}}, 1'b1, 1'b0, 1'b0, 1'b0, PROG_EMPTY_THRESH, PROG_FULL_THRESH, 39'd0, 1'b0})
+            )
+        RS_TDP36K_W36_R9 (
+            .WEN_A1(WREN),
+            .REN_B1(RDEN),
+            .CLK_A1(WRCLK),
+            .CLK_B1(second_clk),
+            .WDATA_A1({WR_DATA[17], WR_DATA[8], WR_DATA[16:9], WR_DATA[7:0]}),
+            .WDATA_A2({WR_DATA[35], WR_DATA[26], WR_DATA[34:27], WR_DATA[25:18]}),
+            .RDATA_A1({EMPTY, ALMOST_EMPTY, PROG_EMPTY, UNDERFLOW, FULL, ALMOST_FULL, PROG_FULL, OVERFLOW}),
+            .RDATA_B1(rd_data),
+            .FLUSH1(RESET),
+            .CLK_A2(WRCLK),
+            .CLK_B2(second_clk)
+        );
+    end
+else if (DATA_WIDTH_WRITE == 5'd18 && DATA_WIDTH_READ == 4'd9)
+    begin
+        wire [17:0] rd_data;
+        assign RD_DATA = {rd_data[16], rd_data[7:0]};
+        // ----------------------------------------------------------Appending 12th bit as dont care bit
+        RS_TDP36K #(
+            .MODE_BITS({sync_fifo, {2{3'b100}}, {2{3'b010}}, 1'b1, 1'b0, 1'b0, 1'b0, 1'b0, PROG_EMPTY_THRESH[10:0], 1'b0, PROG_FULL_THRESH[10:0], 39'd0, 1'b1})
+            )
+        RS_TDP36K_W18_R9 (
+            .WEN_A1(WREN),
+            .REN_B1(RDEN),
+            .CLK_A1(WRCLK),
+            .CLK_B1(second_clk),
+            .WDATA_A1({WR_DATA[17], WR_DATA[8], WR_DATA[16:9], WR_DATA[7:0]}),
+            .RDATA_A1({EMPTY, ALMOST_EMPTY, PROG_EMPTY, UNDERFLOW, FULL, ALMOST_FULL, PROG_FULL, OVERFLOW}),
+            .RDATA_B1(rd_data),
+            .FLUSH1(RESET),
+            .CLK_A2(WRCLK),
+            .CLK_B2(second_clk)
+        );
+    end
+else if (DATA_WIDTH_WRITE == 4'd9 && DATA_WIDTH_READ == 5'd18)
+    begin
+        wire [17:0] rd_data;
+        assign RD_DATA = {rd_data[17], rd_data[15:8], rd_data[16], rd_data[7:0]};
+        RS_TDP36K #(
+            // ----------------------------------------------------------Appending 12th bit as dont care bit
+            .MODE_BITS({sync_fifo, {2{3'b010}}, {2{3'b100}}, 1'b1, 1'b0, 1'b0, 1'b0, 1'b0, PROG_EMPTY_THRESH[10:0], 1'b0, PROG_FULL_THRESH[10:0], 39'd0, 1'b1})
+            )
+        RS_TDP36K_W9_R18 (
+            .WEN_A1(WREN),
+            .REN_B1(RDEN),
+            .CLK_A1(WRCLK),
+            .CLK_B1(second_clk),
+            .WDATA_A1({1'hx, WR_DATA[8], {8{1'hx}}, WR_DATA[7:0]}),
+            .RDATA_A1({EMPTY, ALMOST_EMPTY, PROG_EMPTY, UNDERFLOW, FULL, ALMOST_FULL, PROG_FULL, OVERFLOW}),
+            .RDATA_B1(rd_data),
+            .FLUSH1(RESET),
+            .CLK_A2(WRCLK),
+            .CLK_B2(second_clk)
+        );
+    end
+else if (DATA_WIDTH_WRITE == 5'd18 && DATA_WIDTH_READ == 6'd36)
+    begin
+        RS_TDP36K #(
+            .MODE_BITS({sync_fifo, {2{3'b110}}, {2{3'b010}}, 1'b1, 1'b0, 1'b0, 1'b0, PROG_EMPTY_THRESH, PROG_FULL_THRESH, 39'd0, 1'b0})
+            )
+        RS_TDP36K_W18_R36 (
+            .WEN_A1(WREN),
+            .REN_B1(RDEN),
+            .CLK_A1(WRCLK),
+            .CLK_B1(second_clk),
+            .WDATA_A1(WR_DATA[17:0]),
+            .RDATA_A1({EMPTY, ALMOST_EMPTY, PROG_EMPTY, UNDERFLOW, FULL, ALMOST_FULL, PROG_FULL, OVERFLOW}),
+            .RDATA_B1(RD_DATA[17:0]),
+            .RDATA_B2(RD_DATA[35:18]),
+            .FLUSH1(RESET),
+            .CLK_A2(WRCLK),
+            .CLK_B2(second_clk)
+        );
+    end
+else 
+    begin
+        wire [35:0] rd_data;
+        assign RD_DATA = {rd_data[35], rd_data[33:26], rd_data[34], rd_data[25:18], rd_data[17], rd_data[15:8] ,rd_data[16], rd_data[7:0]};
+        RS_TDP36K #(
+            .MODE_BITS({sync_fifo, {2{3'b110}}, {2{3'b100}}, 1'b1, 1'b0, 1'b0, 1'b0, PROG_EMPTY_THRESH, PROG_FULL_THRESH, 39'd0, 1'b0})
+            )
+        RS_TDP36K_W9_R36 (
+            .WEN_A1(WREN),
+            .REN_B1(RDEN),
+            .CLK_A1(WRCLK),
+            .CLK_B1(second_clk),
+            .WDATA_A1({1'hx, WR_DATA[8], {8{1'hx}}, WR_DATA[7:0]}),
+            .RDATA_A1({EMPTY, ALMOST_EMPTY, PROG_EMPTY, UNDERFLOW, FULL, ALMOST_FULL, PROG_FULL, OVERFLOW}),
+            .RDATA_B1(rd_data[17:0]),
+            .RDATA_B2(rd_data[35:18]),
+            .FLUSH1(RESET),
+            .CLK_A2(WRCLK),
+            .CLK_B2(second_clk)
         );
     end
 
-else if (DATA_WIDTH == 4)
-    begin
-        RS_TDP36K #(
-            .MODE_BITS({SYNC_FIFO1, {4{3'b001}}, 1'b1, 1'b0, 1'b0, 1'b0, PROG_EMPTY_THRESH, PROG_FULL_THRESH, 39'd0, 1'b1})
-            )
-        RS_TDP36K_inst1(
-            .WEN_A1(WREN),
-            .REN_B1(RDEN),
-            .CLK_A1(WRCLK),
-            .CLK_B1(TEMP_CLK),
-            .WDATA_A1(WR_DATA[3:0]),
-            .RDATA_A1({EMPTY, ALMOST_EMPTY, PROG_EMPTY, UNDERFLOW, FULL, ALMOST_FULL, PROG_FULL, OVERFLOW}),
-            .RDATA_B1(RD_DATA[3:0]),
-            .FLUSH1(RESET),
-            .CLK_A2(WRCLK),
-            .CLK_B2(TEMP_CLK)
-        );
-    end
-
-else if (DATA_WIDTH == 2)
-    begin
-        RS_TDP36K #(
-            .MODE_BITS({SYNC_FIFO1, {4{3'b011}}, 1'b1, 1'b0, 1'b0, 1'b0, PROG_EMPTY_THRESH, PROG_FULL_THRESH, 39'd0, 1'b1})
-            )
-        RS_TDP36K_inst1(
-            .WEN_A1(WREN),
-            .REN_B1(RDEN),
-            .CLK_A1(WRCLK),
-            .CLK_B1(TEMP_CLK),
-            .WDATA_A1(WR_DATA[1:0]),
-            .RDATA_A1({EMPTY, ALMOST_EMPTY, PROG_EMPTY, UNDERFLOW, FULL, ALMOST_FULL, PROG_FULL, OVERFLOW}),
-            .RDATA_B1(RD_DATA[1:0]),
-            .FLUSH1(RESET),
-            .CLK_A2(WRCLK),
-            .CLK_B2(TEMP_CLK)
-        );
-    end
-
-else
-    begin
-        RS_TDP36K #(
-            .MODE_BITS({SYNC_FIFO1, {4{3'b101}}, 1'b1, 1'b0, 1'b0, 1'b0, PROG_EMPTY_THRESH, PROG_FULL_THRESH, 39'd0, 1'b1})
-            )
-        RS_TDP36K_inst1(
-            .WEN_A1(WREN),
-            .REN_B1(RDEN),
-            .CLK_A1(WRCLK),
-            .CLK_B1(TEMP_CLK),
-            .WDATA_A1(WR_DATA[0:0]),
-            .RDATA_A1({EMPTY, ALMOST_EMPTY, PROG_EMPTY, UNDERFLOW, FULL, ALMOST_FULL, PROG_FULL, OVERFLOW}),
-            .RDATA_B1(RD_DATA[0:0]),
-            .FLUSH1(RESET),
-            .CLK_A2(WRCLK),
-            .CLK_B2(TEMP_CLK)
-        );
-    end
 endgenerate
 
 endmodule
--- rapidsilicon/ip/fifo_generator/v1_0/sim/FIFO18K.v
+++ rapidsilicon/ip/fifo_generator/v1_0/sim/FIFO18K.v
@@ -1,63 +1,147 @@
 // --------------------------------------------------------------------------
 // ---------------- Copyright (C) 2023 RapidSilicon -------------------------
 // --------------------------------------------------------------------------
-// ---------------------- FIFO18K Primitive ---------------------------------
+// ---------------------- FIFO18KX2 Primitive -------------------------------
 // --------------------------------------------------------------------------
 
-module FIFO18K #(
-    parameter   DATA_WIDTH          = 18,                  // Data Width of FIFO from 1, 2, 4, 9, 18
-    parameter   SYNC_FIFO           = "TRUE",              // Synchronous or Asynchronous FIFO "TRUE"/"FALSE"
-    parameter   PROG_FULL_THRESH    = 12'b100000000000,    // Threshold indicating that the FIFO buffer is considered Full
-    parameter   PROG_EMPTY_THRESH   = 12'b111111111100     // Threshold indicating that the FIFO buffer is considered Empty
+module FIFO18KX2 #(
+    parameter DATA_WRITE_WIDTH1 = 5'd18,        // Write Data Width of FIFO1 from 1, 2, 4, 9, 18
+    parameter DATA_WRITE_READ1 = 5'd18,         // Read Data Width of FIFO1 from 1, 2, 4, 9, 18
+    parameter FIFO_TYPE1 = "SYNCHRONOUS",       // Synchronous or Asynchronous FIFO1     
+    parameter PROG_EMPTY_THRESH1 = 12'h004,     // Threshold indicating that the FIFO1 buffer is considered Empty
+    parameter PROG_FULL_THRESH1 = 12'h8fa,      // Threshold indicating that the FIFO1 buffer is considered Full
+    
+    parameter DATA_WRITE_WIDTH2 = 5'd18,        // Write Data Width of FIFO2 from 1, 2, 4, 9, 18
+    parameter DATA_WRITE_READ2 = 5'd18,         // Read Data Width of FIFO1 from 1, 2, 4, 9, 18
+    parameter FIFO_TYPE2 = "SYNCHRONOUS",       // Synchronous or Asynchronous FIFO2    
+    parameter PROG_EMPTY_THRESH2 = 11'h004,     // Threshold indicating that the FIFO2 buffer is considered Empty
+    parameter PROG_FULL_THRESH2 = 11'h4fa       // Threshold indicating that the FIFO2 buffer is considered Full
 )
 (
-    input wire  [DATA_WIDTH-1:0] WR_DATA,                  // 18-bits Data coming inside FIFO
-    output wire [DATA_WIDTH-1:0] RD_DATA,                  // 18-bits Data coming out from FIFO
-    output wire EMPTY,                                     // 1-bit output: Empty Flag
-    output wire FULL,                                      // 1-bit output: Full Flag
-    output wire OVERFLOW,                                  // 1-bit output: Overflow Flag 
-    output wire UNDERFLOW,                                 // 1-bit output: Underflow Flag
-    input wire  RDEN,                                      // 1-bit input:  Read Enable
-    input wire  WREN,                                      // 1-bit input:  Write Enable
-    output wire ALMOST_EMPTY,                              // 1-bit output: This Flag is asserted when FIFO contains EMPTY plus one data words.
-    output wire ALMOST_FULL,                               // 1-bit output: This Flag is asserted when FIFO contains FULL minus one data words.
-    output wire PROG_EMPTY,                                // 1-bit output: Empty Watermark Flag
-    output wire PROG_FULL,                                 // 1-bit output: Full Watermark Flag
-    input wire  WRCLK,                                     // 1-bit input:  Write Clock
-    input wire  RDCLK,                                     // 1-bit input:  Read Clock
-    input wire  RESET                                      // 1-bit input:  Active Low Synchronous Reset
+    // -------------Ports for FIFO 1-----------------
+    input wire RESET1,                          // 1-bit input:  Active Low Synchronous Reset
+    input wire WR_CLK1,                         // 1-bit input:  Write Clock
+    input wire RD_CLK1,                         // 1-bit input:  Read Clock
+    input wire RD_EN1,                          // 1-bit input:  Read Enable
+    input wire WR_EN1,                          // 1-bit input:  Write Enable
+    input wire [DATA_WRITE_WIDTH1-1:0] WR_DATA1,// DATA_WIDTH1-bits Data coming inside FIFO
+    output wire [DATA_WRITE_READ1-1:0] RD_DATA1,// DATA_WIDTH1-bits Data coming out from FIFO
+    output wire EMPTY1,                         // 1-bit output: Empty Flag
+    output wire FULL1,                          // 1-bit output: Full Flag
+    output wire ALMOST_EMPTY1,                  // 1-bit output: This Flag is asserted when FIFO contains EMPTY plus one data words
+    output wire ALMOST_FULL1,                   // 1-bit output: This Flag is asserted when FIFO contains FULL minus one data words
+    output wire PROG_EMPTY1,                    // 1-bit output: Empty Watermark Flag
+    output wire PROG_FULL1,                     // 1-bit output: Full Watermark Flag
+    output wire OVERFLOW1,                      // 1-bit output: Overflow Flag 
+    output wire UNDERFLOW1,                     // 1-bit output: Underflow Flag
+
+    // -------------Ports for FIFO 2-----------------
+    input wire RESET2,                          // 1-bit input:  Active Low Synchronous Reset
+    input wire WR_CLK2,                         // 1-bit input:  Write Clock
+    input wire RD_CLK2,                         // 1-bit input:  Read Clock
+    input wire RD_EN2,                          // 1-bit input:  Read Enable
+    input wire WR_EN2,                          // 1-bit input:  Write Enable
+    input wire [DATA_WRITE_WIDTH2-1:0] WR_DATA2,// DATA_WIDTH2-bits Data coming inside FIFO
+    output wire [DATA_WRITE_READ2-1:0] RD_DATA2,// DATA_WIDTH2-bits Data coming out from FIFO
+    output wire EMPTY2,                         // 1-bit output: Empty Flag
+    output wire FULL2,                          // 1-bit output: Full Flag
+    output wire ALMOST_EMPTY2,                  // 1-bit output: This Flag is asserted when FIFO contains EMPTY plus one data words
+    output wire ALMOST_FULL2,                   // 1-bit output: This Flag is asserted when FIFO contains FULL minus one data words
+    output wire PROG_EMPTY2,                    // 1-bit output: Empty Watermark Flag
+    output wire PROG_FULL2,                     // 1-bit output: Full Watermark Flag
+    output wire OVERFLOW2,                      // 1-bit output: Overflow Flag 
+    output wire UNDERFLOW2                      // 1-bit output: Underflow Flag
 );
 
-initial 
-begin
-    if (!(DATA_WIDTH == 1 || DATA_WIDTH == 2 || DATA_WIDTH == 4 || DATA_WIDTH == 9 || DATA_WIDTH == 18)) 
-        begin
-        $error("Incorrect DATA_WIDTH: %0d\nEnter valid DATA_WIDTH: 1, 2, 4, 8, 9, 18", DATA_WIDTH);
-        $finish;
-        end
+localparam data_width_write1 = (DATA_WRITE_WIDTH1 > 4'd9) ? 5'd18 : DATA_WRITE_WIDTH1;
+localparam data_width_write2 = (DATA_WRITE_WIDTH2 > 4'd9) ? 5'd18 : DATA_WRITE_WIDTH2;
+localparam data_width_read1 = (DATA_WRITE_READ1 > 4'd9) ? 5'd18 : DATA_WRITE_READ1;
+localparam data_width_read2 = (DATA_WRITE_READ2 > 4'd9) ? 5'd18 : DATA_WRITE_READ2;
+
+initial begin
+    if ((DATA_WRITE_WIDTH1 < 1'd1) || (DATA_WRITE_WIDTH1 > 5'd18)) begin
+       $display("FIFO18KX2 instance %m DATA_WRITE_WIDTH1 set to incorrect value, %d.  Values must be between 1 and 18.", DATA_WRITE_WIDTH1);
+    #1 $stop;
+    end
+    if ((DATA_WRITE_READ1 < 1'd1) || (DATA_WRITE_READ1 > 5'd18)) begin
+       $display("FIFO18KX2 instance %m DATA_WRITE_READ1 set to incorrect value, %d.  Values must be between 1 and 18.", DATA_WRITE_READ1);
+    #1 $stop;
+    end
+    case(FIFO_TYPE1)
+      "SYNCHRONOUS" ,
+      "ASYNCHRONOUS": begin end
+      default: begin
+        $display("\nError: FIFO18KX2 instance %m has parameter FIFO_TYPE1 set to %s.  Valid values are SYNCHRONOUS, ASYNCHRONOUS\n", FIFO_TYPE1);
+        #1 $stop ;
+      end
+    endcase
+
+    if ((DATA_WRITE_WIDTH2 < 1'd1) || (DATA_WRITE_WIDTH2 > 5'd18)) begin
+       $display("FIFO18KX2 instance %m DATA_WRITE_WIDTH2 set to incorrect value, %d.  Values must be between 1 and 18.", DATA_WRITE_WIDTH2);
+    #1 $stop;
+    end
+    if ((DATA_WRITE_READ2 < 1'd1) || (DATA_WRITE_READ2 > 5'd18)) begin
+       $display("FIFO18KX2 instance %m DATA_WRITE_READ2 set to incorrect value, %d.  Values must be between 1 and 18.", DATA_WRITE_READ2);
+    #1 $stop;
+    end
+    case(FIFO_TYPE2)
+      "SYNCHRONOUS" ,
+      "ASYNCHRONOUS": begin end
+      default: begin
+        $display("\nError: FIFO18KX2 instance %m has parameter FIFO_TYPE2 set to %s.  Valid values are SYNCHRONOUS, ASYNCHRONOUS\n", FIFO_TYPE2);
+        #1 $stop ;
+      end
+    endcase
 end
 
 FIFO #(
-    .DATA_WIDTH(DATA_WIDTH),
-    .SYNC_FIFO(SYNC_FIFO),
-    .PROG_FULL_THRESH(PROG_FULL_THRESH),
-    .PROG_EMPTY_THRESH(PROG_EMPTY_THRESH)
+    .DATA_WIDTH_WRITE(data_width_write1),
+    .DATA_WIDTH_READ(data_width_read1),
+    .SYNC_FIFO(FIFO_TYPE1),
+    .PROG_FULL_THRESH(PROG_FULL_THRESH1),
+    .PROG_EMPTY_THRESH(PROG_EMPTY_THRESH1)
 )
-FIFO_insta_1(
-    .WR_DATA(WR_DATA),
-    .RD_DATA(RD_DATA),
-    .EMPTY(EMPTY),
-    .FULL(FULL),
-    .ALMOST_EMPTY(ALMOST_EMPTY),
-    .ALMOST_FULL(ALMOST_FULL),
-    .PROG_EMPTY(PROG_EMPTY),
-    .PROG_FULL(PROG_FULL),
-    .OVERFLOW(OVERFLOW),
-    .UNDERFLOW(UNDERFLOW),
-    .RDEN(RDEN),
-    .WREN(WREN),
-    .WRCLK(WRCLK),
-    .RDCLK(RDCLK),
-    .RESET(RESET)
+FIFO18K_1 (
+    .WR_DATA(WR_DATA1),
+    .RD_DATA(RD_DATA1),
+    .EMPTY(EMPTY1),
+    .FULL(FULL1),
+    .OVERFLOW(OVERFLOW1),
+    .UNDERFLOW(UNDERFLOW1),
+    .RDEN(RD_EN1),
+    .WREN(WR_EN1),
+    .ALMOST_EMPTY(ALMOST_EMPTY1),
+    .ALMOST_FULL(ALMOST_FULL1),
+    .PROG_EMPTY(PROG_EMPTY1),
+    .PROG_FULL(PROG_FULL1),
+    .WRCLK(WR_CLK1),
+    .RDCLK(RD_CLK1),
+    .RESET(RESET1)
 );
+
+FIFO #(
+    .DATA_WIDTH_WRITE(data_width_write2),
+    .DATA_WIDTH_READ(data_width_read2),
+    .SYNC_FIFO(FIFO_TYPE2),
+    .PROG_FULL_THRESH(PROG_FULL_THRESH2),
+    .PROG_EMPTY_THRESH(PROG_EMPTY_THRESH2)
+)
+FIFO18K_2 (
+    .WR_DATA(WR_DATA2),
+    .RD_DATA(RD_DATA2),
+    .EMPTY(EMPTY2),
+    .FULL(FULL2),
+    .OVERFLOW(OVERFLOW2),
+    .UNDERFLOW(UNDERFLOW2),
+    .RDEN(RD_EN2),
+    .WREN(WR_EN2),
+    .ALMOST_EMPTY(ALMOST_EMPTY2),
+    .ALMOST_FULL(ALMOST_FULL2),
+    .PROG_EMPTY(PROG_EMPTY2),
+    .PROG_FULL(PROG_FULL2),
+    .WRCLK(WR_CLK2),
+    .RDCLK(RD_CLK2),
+    .RESET(RESET2)
+);
+
 endmodule
