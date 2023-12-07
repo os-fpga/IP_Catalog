0810fb35885c6daf60c136e00847be7d94d917f5
sarmadsalman-RS <sarmad.salman@rapidsilicon.com>
2023-12-07 17:54:14 +0500

    redundant data removed

--- rapidsilicon/ip/fir_generator/v1_0/rapidsilicon/ip/fir_generator/v1_0/FIR_generator/sim/README.md
+++
@@ -1,5 +0,0 @@
-# Simulations
-To run the simulations for the FIR Generator core, run the following command on a terminal:
-```
-make OUT_DIR=$(PWD) MODULE_NAME=<name_of_generated_IP_module>
-```
--- rapidsilicon/ip/fir_generator/v1_0/rapidsilicon/ip/fir_generator/v1_0/FIR_generator/synth/raptor.tcl
+++
@@ -1,8 +0,0 @@
-create_design FIR_generator
-target_device GEMINI
-add_include_path ../src
-add_library_path ../src
-add_library_ext .v .sv
-add_design_file ../src/FIR_generator.v
-set_top_module FIR_generator
-synthesize
