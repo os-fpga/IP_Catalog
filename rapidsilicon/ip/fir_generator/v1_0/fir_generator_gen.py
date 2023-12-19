67983c1023bb160bc93ed063148da9ed5480294f
sarmadsalman-RS <sarmad.salman@rapidsilicon.com>
2023-12-19 11:40:56 +0500

    fixed CI bug

--- rapidsilicon/ip/fir_generator/v1_0/fir_generator_gen.py
+++ rapidsilicon/ip/fir_generator/v1_0/fir_generator_gen.py
@@ -76,7 +76,7 @@ def main():
     # Core bool value parameters.
     core_bool_param_group = parser.add_argument_group(title="Core bool parameters")
     core_bool_param_group.add_argument("--coefficients_file",        type=bool,   default=True,     help="Enter Coefficients manually or select a text file containing them")
-    core_file_path_group.add_argument("--file_path",    type=str,   default="./",   help="Text file for Coefficients of the FIR Filter")
+    core_file_path_group.add_argument("--file_path",    type=str,   default="./src/coefficients.txt",   help="Text file for Coefficients of the FIR Filter")
 
     # Build Parameters.
     build_group = parser.add_argument_group(title="Build parameters")
