8124adeef5793ebded86245853360e7c8d6610e4
Bilal Ahmed <96869856+bilalahmed-RS@users.noreply.github.com>
2024-01-04 10:15:05 +0500

    Update install_ubuntu_dependencies_build.sh

--- .github/install_ubuntu_dependencies_build.sh
+++ .github/install_ubuntu_dependencies_build.sh
@@ -1,5 +1,6 @@
 sudo apt-get update
 sudo apt install -y cmake build-essential
+sudo apt-get install iverilog
 #sudo apt install -y python3 python3-pip
 #python3 -m pip install --upgrade pip
 #python3 -m pip install pipenv
