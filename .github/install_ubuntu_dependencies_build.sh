8bad1c1bc8c3776f432a0e4cae72a794258e5b46
Bilal Ahmed <96869856+bilalahmed-RS@users.noreply.github.com>
2024-01-04 10:49:04 +0500

    Update install_ubuntu_dependencies_build.sh

--- .github/install_ubuntu_dependencies_build.sh
+++ .github/install_ubuntu_dependencies_build.sh
@@ -4,5 +4,9 @@ sudo apt-get install iverilog
 #sudo apt install -y python3 python3-pip
 #python3 -m pip install --upgrade pip
 #python3 -m pip install pipenv
+python3 -m pip install cocotb==1.7.1
+python3 -m pip install cocotb-bus=0.1.1
+python3 -m pip install cocotb-test==0.2.1
+python3 -m pip install cocotbext-axi==0.1.18
 sudo ln -sf /usr/lib/x86_64-linux-gnu/libffi.so.7.1.0 /usr/lib/x86_64-linux-gnu/libffi.so.6
 
