12d77d7ead3615f8d30bbc5028b39c305eaf0dcb
sarmadsalman-RS <sarmad.salman@rapidsilicon.com>
2024-01-08 15:05:19 +0500

    append version to generated verilog file

--- rapidsilicon/ip/axi_cdma/v2_0/axi_cdma_gen.py
+++ rapidsilicon/ip/axi_cdma/v2_0/axi_cdma_gen.py
@@ -176,7 +176,7 @@ def main():
         ip_version = "00000000_00000000_0000000000000001"
         ip_version = ("32'h{}").format(hex(int(ip_version, 2))[2:])
         
-        wrapper = os.path.join(args.build_dir, "rapidsilicon", "ip", "axi_cdma", "v2_0", args.build_name, "src",args.build_name+".v")
+        wrapper = os.path.join(args.build_dir, "rapidsilicon", "ip", "axi_cdma", "v2_0", args.build_name, "src",args.build_name + "_" + "v2_0" + ".v")
         new_lines = []
         with open (wrapper, "r") as file:
             lines = file.readlines()
