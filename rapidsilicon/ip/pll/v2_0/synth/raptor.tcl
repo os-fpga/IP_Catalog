1881d0853c8c5333af192052470b9e2211993fe2
bilal458 <bilalahmed458@gmail.com>
2024-02-12 12:17:54 +0500

    Boot clock updated

--- rapidsilicon/ip/pll/v2_0/synth/raptor.tcl
+++
@@ -1,8 +0,0 @@
-create_design pll_wrapper
-target_device GEMINI
-add_include_path ../src
-add_library_path ../src
-add_library_ext .v .sv
-add_design_file ../src/pll_wrapper.v
-set_top_module pll_wrapper
-synthesize
