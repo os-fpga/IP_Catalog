4fdf9be84b6e02cc4a8ace87d3875d98e7eeb116
Sarmad Salman <106231344+Sarmad-Salman@users.noreply.github.com>
2024-05-31 16:57:36 +0500

    dynamic updating of signed and unsigned variables in fir_golden

--- rapidsilicon/ip/fir_generator/v1_0/fir_generator_gen.py
+++ rapidsilicon/ip/fir_generator/v1_0/fir_generator_gen.py
@@ -150,10 +150,6 @@ def main():
     if args.json:
         args = rs_builder.import_args_from_json(parser=parser, json_filename=args.json)
         rs_builder.import_ip_details_json(build_dir=args.build_dir ,details=details , build_name = args.build_name, version = "v1_0")
-        file_path = os.path.dirname(os.path.realpath(__file__))
-        rs_builder.copy_images(file_path)
-
-
         if (args.optimization == "Area"):
             if (not args.coefficients_file):
                 option_strings_to_remove = ['--number_of_coefficients']
@@ -257,7 +253,6 @@ def main():
     # Export JSON Template (Optional) --------------------------------------------------------------
     if args.json_template:
         rs_builder.export_json_template(parser=parser, dep_dict=dep_dict, summary=summary)
-
 
     # Create Generator -------------------------------------------------------------------------------
     platform = OSFPGAPlatform(io=[], toolchain="raptor", device="gemini")
@@ -402,10 +397,17 @@ end
             lines = files.readlines()
         for i, value in enumerate(extract_numbers(args.coefficients, args.coefficients_file)):
             insert_line = f"\tassign coeff[{i}] = {decimal_to_fixed_point(value, args.coefficient_width - args.coefficient_fractional_bits, args.coefficient_fractional_bits, args.signed)};\n"
-            lines.insert(19 - 1 + i, insert_line)
+            lines.insert(21 - 1 + i, insert_line)
 
         with open(file, 'w') as files:
             files.writelines(lines)
+
+        if (not args.signed):
+            with open(file, 'r') as files:
+                content = files.read()
+            modified_content = content.replace('signed', '')
+            with open(file, 'w') as files:
+                files.write(modified_content)
 
 if __name__ == "__main__":
     main()
